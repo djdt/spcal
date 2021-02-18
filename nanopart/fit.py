803b7dc68cdc60a92af3623ed928aeb80906ae47
djdt <10266332+djdt@users.noreply.github.com>
2021-02-18 17:56:52 +1100

    no lognorm for u

--- nanopart/fit.py
+++ nanopart/fit.py
@@ -7,12 +7,12 @@ _s2 = np.sqrt(2.0)
 _s2pi = np.sqrt(2.0 * np.pi)
 
 
-def normal_pdf(x: np.ndarray, sigma: float, mu: float) -> np.ndarray:
+def normal_pdf(x: np.ndarray, mu: float, sigma: float) -> np.ndarray:
     return 1.0 / (sigma * _s2pi) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
 
 
-def lognormal_pdf(x: np.ndarray, sigma: float, mu: float) -> np.ndarray:
-    return 1.0 / (x * sigma * _s2pi) * np.exp(-0.5 * ((np.log(x) - mu) / sigma) ** 2)
+# def lognormal_pdf(x: np.ndarray, mu: float, sigma: float) -> np.ndarray:
+#     return 1.0 / (x * sigma * _s2pi) * np.exp(-0.5 * ((np.log(x) - mu) / sigma) ** 2)
 
 
 def fit_pdf(
@@ -39,11 +39,28 @@ def fit_normal(x: np.ndarray, y: np.ndarray, n: int = 100):
     mu = np.linspace(x.min(), x.max(), n)
     sigma = np.linspace(1e-3, 10.0, n)
 
-    return fit_pdf(x, y, normal_pdf, [sigma, mu])
+    return fit_pdf(x, y, normal_pdf, [mu, sigma])
 
 
-def fit_lognormal(x: np.ndarray, y: np.ndarray, n: int = 100):
-    mu = np.log(np.linspace(x.min(), x.max(), n))
-    sigma = np.linspace(1e-3, 1.0, n)
+# def fit_lognormal(x: np.ndarray, y: np.ndarray, n: int = 100):
+#     mu = np.linspace(0, np.log(x.max()), n)
+#     print(mu)
+#     sigma = np.linspace(1e-3, 10.0, n)
+#     loc = np.linspace(x.min(), x.max(), n)
 
-    return fit_pdf(x, y, lognormal_pdf, [sigma, mu])
+#     return fit_pdf(x, y, lognormal_pdf, [mu, sigma, loc])
+
+
+# import matplotlib.pyplot as plt
+
+
+# x = np.random.lognormal(0.1, 1.0, 1000) + 50.0
+
+# c, bins, _ = plt.hist(x, bins=64, density=True)
+
+# fit, err, opt = fit_lognormal(bins[1:], c)
+# print(opt)
+
+# plt.plot(bins[1:], fit)
+
+# plt.show()
--- nanopart/gui/npresults.py
+++ nanopart/gui/npresults.py
@@ -4,7 +4,7 @@ from PySide2.QtCharts import QtCharts
 import numpy as np
 
 import nanopart
-from nanopart.fit import fit_normal, fit_lognormal
+from nanopart.fit import fit_normal
 
 from nanopart.gui.charts import ParticleHistogram
 from nanopart.gui.npoptions import NPOptionsWidget
@@ -94,9 +94,9 @@ class NPResultsWidget(QtWidgets.QWidget):
         hist, bins2 = np.histogram(
             data, bins=256, range=(data.min(), data.max()), density=True
         )
-        result_log = fit_lognormal(bins2[1:], hist)
-        result_norm = fit_normal(bins2[1:], hist)
-        fit, err, opts = result_log if result_log[1] < result_norm[1] else result_norm
+        # result_log = fit_lognormal(bins2[1:], hist)
+        fit, _, _ = fit_normal(bins2[1:], hist)
+        # fit, err, opts = result_log if result_log[1] < result_norm[1] else result_norm
         fit = fit * (bins[1] - bins[0]) * data.size
         self.chart.setFit(bins2[1:], fit)
 
