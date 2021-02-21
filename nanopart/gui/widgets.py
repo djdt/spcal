9b8ee9d2b617cdd29bec8930c69f3111864cbce4
djdt <10266332+djdt@users.noreply.github.com>
2021-02-21 19:22:11 +1100

    add elided label

--- nanopart/gui/widgets.py
+++ nanopart/gui/widgets.py
@@ -3,6 +3,38 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from typing import Tuple
 
 
+class ElidedLabel(QtWidgets.QFrame):
+    def __init__(self, text: str = "", parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
+        self._text = text
+        self._elide = QtCore.Qt.ElideLeft
+
+    def elide(self) -> QtCore.Qt.TextElideMode:
+        return self._elide
+
+    def setElide(self, elide: QtCore.Qt.TextElideMode) -> None:
+        self._elide = elide
+
+    def text(self) -> str:
+        return self._text
+
+    def setText(self, text: str) -> None:
+        self._text = text
+
+    def paintEvent(self, event: QtGui.QPaintEvent) -> None:
+        painter = QtGui.QPainter(self)
+        fm = painter.fontMetrics()
+
+        elided = fm.elidedText(self._text, self._elide, self.width())
+        painter.drawText(
+            self.contentsRect(),
+            QtCore.Qt.AlignVCenter
+            | QtCore.Qt.TextSingleLine
+            | QtCore.Qt.TextShowMnemonic,
+            elided,
+        )
+
+
 class RangeSlider(QtWidgets.QSlider):
     value2Changed = QtCore.Signal(int)
 
