504821b6f4984511de9650063b4773e8d846d153
djdt <10266332+djdt@users.noreply.github.com>
2021-08-04 14:57:23 +1000

    revert removal of centers, used for drawing

--- spcal/gui/inputs.py
+++ spcal/gui/inputs.py
@@ -255,11 +255,11 @@ class InputWidget(QtWidgets.QWidget):
             self.background_count.setText("")
             self.lod_count.setText("")
         else:
-            detections, labels, _ = spcal.accumulate_detections(
+            detections, labels, regions = spcal.accumulate_detections(
                 responses, self.limits[2], self.limits[3]
             )
-            # centers = (regions[:, 0] + regions[:, 1]) // 2
-            # self.centers = centers
+            centers = (regions[:, 0] + regions[:, 1]) // 2
+            self.centers = centers
             # values = np.linspace(0, responses.size, 3 + 1)
             # indicies = np.searchsorted(self.centers, values, side="left")
 
