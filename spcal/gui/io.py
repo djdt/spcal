3f8f1cbe577f612f5f0c07dada2fbf2a8a74c183
djdt <10266332+djdt@users.noreply.github.com>
2022-10-02 21:19:13 +1100

    import now works

--- spcal/gui/io.py
+++ spcal/gui/io.py
@@ -1,9 +1,12 @@
 from PySide2 import QtCore, QtGui, QtWidgets
+import numpy as np
 
-from typing import Generator, Optional
+from typing import List, Optional
 
 
 class ImportDialog(QtWidgets.QDialog):
+    dataImported = QtCore.Signal(np.ndarray)
+
     def __init__(self, file: str, parent: Optional[QtWidgets.QWidget] = None):
         super().__init__(parent)
         self.resize(800, 800)
@@ -11,8 +14,11 @@ class ImportDialog(QtWidgets.QDialog):
 
         header_row_count = 10
 
-        self.file_data = self.delimited_translated_columns(file)
-        self.file_header = [x for _, x in zip(range(header_row_count), self.file_data)]
+        # self.file_data = self.delimited_translated_columns(file)
+        self.file_pointer = open(file, "r")
+        self.file_header = [
+            x for _, x in zip(range(header_row_count), self.file_pointer)
+        ]
 
         first_data_line = 0
         for line in self.file_header:
@@ -28,61 +34,87 @@ class ImportDialog(QtWidgets.QDialog):
         self.table = QtWidgets.QTableWidget()
         self.table.setColumnCount(column_count)
         self.table.setRowCount(header_row_count)
+        self.table.setFont(QtGui.QFont("Courier"))
+
+        self.combo_delimiter = QtWidgets.QComboBox()
+        self.combo_delimiter.addItems([",", ";", "Space", "Tab"])
+        self.combo_delimiter.currentIndexChanged.connect(self.fillTable)
 
         self.spinbox_first_line = QtWidgets.QSpinBox()
-        self.spinbox_first_line.setRange(0, header_row_count - 1)
+        self.spinbox_first_line.setRange(1, header_row_count - 1)
         self.spinbox_first_line.setValue(first_data_line)
-        self.spinbox_first_line.valueChanged.connect(self.fill_table)
+        self.spinbox_first_line.valueChanged.connect(self.fillTable)
 
         self.le_ignore_columns = QtWidgets.QLineEdit()
-        self.le_ignore_columns.setText("1,")
-        self.le_ignore_columns.setValidator(QtGui.QRegularExpressionValidator(QtCore.QRegularExpression("[0-9,]+")))
-        self.le_ignore_columns.textChanged.connect(self.fill_table)
+        self.le_ignore_columns.setText("1;")
+        self.le_ignore_columns.setValidator(
+            QtGui.QRegularExpressionValidator(QtCore.QRegularExpression("[0-9;]+"))
+        )
+        self.le_ignore_columns.textChanged.connect(self.fillTable)
+
+        self.button_box = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        )
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
 
         controls = QtWidgets.QFormLayout()
+        controls.addRow("Delimiter:", self.combo_delimiter)
         controls.addRow("Import From Row:", self.spinbox_first_line)
         controls.addRow("Ignore Columns:", self.le_ignore_columns)
 
-        self.fill_table()
+        self.fillTable()
 
         layout = QtWidgets.QVBoxLayout()
         layout.addLayout(controls)
         layout.addWidget(self.table)
+        layout.addWidget(self.button_box)
         self.setLayout(layout)
 
-    def fill_table(self) -> None:
-        ignores = [int(i or 0) - 1 for i in self.le_ignore_columns.text().split(",")]
-        for row, line in enumerate(self.file_header):
-            for col, text in enumerate(line.split(",")):
-                item = QtWidgets.QTableWidgetItem(text)
-                if row < self.spinbox_first_line.value() - 1 or col in ignores:
+    def delimiter(self) -> str:
+        delimiter = self.combo_delimiter.currentText()
+        if delimiter == "Space":
+            delimiter = " "
+        elif delimiter == "Tab":
+            delimiter = "\t"
+        return delimiter
+
+    def ignoreColumns(self) -> List[int]:
+        return [int(i or 0) - 1 for i in self.le_ignore_columns.text().split(";")]
+
+    def fillTable(self) -> None:
+        lines = [line.split(self.delimiter()) for line in self.file_header]
+        self.table.setColumnCount(max(len(line) for line in lines))
+        for row, line in enumerate(lines):
+            for col, text in enumerate(line):
+                item = QtWidgets.QTableWidgetItem(text.strip())
+                if row != self.spinbox_first_line.value() - 1:
+                    item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+                if (
+                    row < self.spinbox_first_line.value() - 1
+                    or col in self.ignoreColumns()
+                ):
                     item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEnabled)
-                item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+
                 self.table.setItem(row, col, item)
         self.table.resizeColumnsToContents()
 
-    def delimited_translated_columns(
-        self, file: str, columns: int = 1
-    ) -> Generator[str, None, None]:
-        """Translates inputs with ';' to have ',' as delimiter and '.' as decimal.
-        Ensures at least `columns` columns in data by prepending ','."""
-        map = str.maketrans({";": ",", ",": "."})
-        with open(file, "r") as fp:
-            for line in fp:
-                if ";" in line:
-                    line = line.translate(map)
-                count = line.count(",")
-                if count < columns:
-                    yield "," * (columns - count - 1) + line
-                else:
-                    yield line
+    def accept(self) -> None:
+        self.file_pointer.seek(0)
 
+        ignores = self.ignoreColumns()
+        cols = [col for col in range(self.table.columnCount()) if col not in ignores]
+        header_row = self.spinbox_first_line.value() - 1
+        headers = [self.table.item(header_row, col).text() for col in cols]
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication()
-    id = ImportDialog("/home/tom/Downloads/AuAg.csv")
-    # id = ImportDialog(
-    #     "/home/tom/MEGA/Uni/Experimental/ICPMS/Mn Single Cell/20211216_algae/20211216_algae_c.b/algae.d/algae.csv"
-    # )
-    id.show()
-    app.exec_()
+        data = np.genfromtxt(
+            self.file_pointer,
+            delimiter=self.delimiter(),
+            usecols=cols,
+            names=headers,
+            skip_header=header_row + 1,
+            converters={0: lambda s: float(s.replace(",", "."))},
+            invalid_raise=False,
+        )
+        self.dataImported.emit(data)
+        super().accept()
