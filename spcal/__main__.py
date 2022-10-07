103fd6a31f517d4ebf7e18d8f1763c07aabb6d11
djdt <10266332+djdt@users.noreply.github.com>
2022-10-07 22:15:26 +1100

    addd nohook

--- spcal/__main__.py
+++ spcal/__main__.py
@@ -15,26 +15,30 @@ logger.setLevel(logging.INFO)
 
 
 def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
+    def existing_file(file: str) -> Path:
+        path = Path(file)
+        if not path.exists():
+            raise argparse.ArgumentTypeError(f"file {file} does not exist")
+        return path
+
     parser = argparse.ArgumentParser(
         prog="spcal",
         description="spICP-MS toolkit and visulistion.",
     )
 
     parser.add_argument(
-        "--sample", type=Path, help="Open file as sample on startup."
+        "--sample", type=existing_file, help="open file as sample on startup"
     )
     parser.add_argument(
-        "--reference", type=Path, help="Open file as reference on startup."
+        "--reference", type=existing_file, help="open file as reference on startup"
     )
     parser.add_argument(
-        "qtargs", nargs=argparse.REMAINDER, help="Arguments to pass to Qt."
+        "--nohook", action="store_true", help="don't install the execption hook"
+    )
+    parser.add_argument(
+        "qtargs", nargs=argparse.REMAINDER, help="arguments to pass to Qt"
     )
     args = parser.parse_args(argv)
-
-    if args.sample is not None and not args.sample.exists():
-        raise parser.error(f"[--sample]: File '{args.sample}' not found.")
-    if args.reference is not None and not args.reference.exists():
-        raise parser.error(f"[--reference]: File '{args.reference}' not found.")
 
     return args
 
@@ -48,16 +52,18 @@ def main(argv: Optional[List[str]] = None) -> int:
 
     window = NanoPartWindow()
 
-    sys.excepthook = window.exceptHook
+    if not args.nohook:
+        sys.excepthook = window.exceptHook
+
     logger.addHandler(window.log.handler)
     logger.info(f"SPCal {__version__} started.")
 
     window.show()
 
     if args.sample:
-        window.sample.loadFile(args.sample)
+        window.sample.dialogLoadFile(args.sample)
     if args.reference:
-        window.reference.loadFile(args.reference)
+        window.reference.dialogLoadFile(args.reference)
 
     # Keep event loop active with timer
     timer = QtCore.QTimer()
