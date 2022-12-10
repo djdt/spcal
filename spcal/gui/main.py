876552bb5772cbeccb0bc1ac58f262ffc9c73060
djdt <10266332+djdt@users.noreply.github.com>
2022-12-10 19:12:06 +1100

    NanoPart -> SPCal

--- spcal/gui/main.py
+++ spcal/gui/main.py
@@ -17,7 +17,7 @@ from spcal.gui.util import create_action
 logger = logging.getLogger(__name__)
 
 
-class NanoPartWindow(QtWidgets.QMainWindow):
+class SPCalWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("SPCal")
