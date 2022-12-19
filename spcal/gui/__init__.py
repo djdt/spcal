8502894cf95e438be49208c314df42925b8a01b6
djdt <10266332+djdt@users.noreply.github.com>
2022-12-19 18:35:58 +1100

    add icons init to gui

--- spcal/gui/__init__.py
+++ spcal/gui/__init__.py
@@ -1,1 +1,5 @@
- 
+from PySide6 import QtCore, QtGui, QtWidgets
+
+# Set Some Qt attributes
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+QtGui.QIcon.setThemeName("breeze")
