0bb975ddcd1636515ac2fa7f49188227fb33269c
djdt <10266332+djdt@users.noreply.github.com>
2022-12-22 12:05:07 +1100

    test for results

--- tests/test_spcalresult.py
+++ tests/test_spcalresult.py
@@ -1,51 +1,94 @@
 import numpy as np
+import pytest
 from numpy.lib import stride_tricks
 
+from spcal.limit import SPCalLimit
+from spcal.particle import particle_size
 from spcal.result import SPCalResult
 
+signal = np.random.random(50)
+signal[5:10] = 10.0
+signal[15:18] = 20.0
+signal[34:38] = 30.0
+detections = np.array([10 * 5, 20 * 3, 30 * 4, 0])
+labels = np.zeros(50)
+labels[signal > 5] = 1
+labels[45:46] = 1
 
 
-# def test_results_from_mass_response():
-#     detections = np.array([10.0, 20.0, 30.0])
-#     r = SPCalResult(np.arra([]), detections=detections, labels=np.array([]), )
-#     results = calc.results_from_mass_response(
-#         detections,
-#         background=1.0,
-#         density=0.01,
-#         lod=5.0,
-#         mass_fraction=0.5,
-#         mass_response=1e-3,
-#     )
-#     assert all(
-#         x in results
-#         for x in ["masses", "sizes", "background_size", "lod", "lod_mass", "lod_size"]
-#     )
-#     assert np.all(results["masses"] == detections * 1e-3 / 0.5)
-#     assert results["lod_mass"] == 5.0 * 1e-3 / 0.5
-#     # Rest are calculated as per particle module
+def test_spcalresult():
+    result = SPCalResult(
+        "test.csv",
+        signal,
+        detections=detections,
+        labels=labels,
+        limits=SPCalLimit(1.0, 5.0, 10.0, "Limit", {}),
+    )
+    assert np.all(result.indicies == [0, 1, 2])
+    assert result.events == 50
+    assert result.number == 3
+    assert result.number_error == np.sqrt(3)
+    assert np.isclose(result.background, np.mean(signal[labels == 0]))
+    assert np.isclose(result.background_error, np.std(signal[labels == 0]))
+
+    assert result.ionic_background is None
+    assert result.number_concentration is None
+    assert result.mass_concentration is None
+
+    assert result.asCellConcentration(1.0) is None
+    assert result.asMass(1.0) is None
+    assert result.asSize(1.0) is None
+
+    with pytest.raises(ValueError):
+        result.fromMassResponse()
+    with pytest.raises(ValueError):
+        result.fromNebulisationEfficiency()
 
 
-# def test_results_from_nebulisation_efficieny():
-#     detections = np.array([10.0, 20.0, 30.0])
-#     results = calc.results_from_nebulisation_efficiency(
-#         detections,
-#         background=1.0,
-#         density=0.01,
-#         lod=5.0,
-#         dwelltime=1e-3,
-#         efficiency=0.05,
-#         uptake=0.2,
-#         time=60.0,
-#         response=100.0,
-#         mass_fraction=0.5,
-#     )
-#     assert all(
-#         x in results
-#         for x in ["masses", "sizes", "background_size", "lod", "lod_mass", "lod_size"]
-#     )
-#     assert all(
-#         x in results
-#         for x in ["concentration", "number_concentration", "background_concentration"]
-#     )
-#     assert results["background_concentration"] == 1.0 / 100.0
-#     # Rest are calculated as per particle module
+def test_spcalresult_from_mass_response():
+    result = SPCalResult(
+        "test.csv",
+        signal,
+        detections=detections,
+        labels=labels,
+        limits=SPCalLimit(1.0, 5.0, 10.0, "Limit", {}),
+        inputs_kws={"density": 0.01, "mass_response": 1e-3, "mass_fraction": 0.5},
+    )
+    result.fromMassResponse()
+
+    assert result.ionic_background is None
+    assert result.number_concentration is None
+    assert result.mass_concentration is None
+
+    assert result.asMass(1.0) == 1.0 * 1e-3 / 0.5
+    assert result.asSize(1.0) == particle_size(1.0 * 1e-3 / 0.5, 0.01)
+
+    assert np.all(result.detections["mass"] == result.asMass(detections))
+    assert np.all(result.detections["size"] == result.asSize(detections))
+
+
+def test_spcalresult_from_nebulisation_efficiency():
+    result = SPCalResult(
+        "test.csv",
+        signal,
+        detections=detections,
+        labels=labels,
+        limits=SPCalLimit(1.0, 5.0, 10.0, "Limit", {}),
+        inputs_kws={
+            "density": 0.01,
+            "dwelltime": 1e-3,
+            "efficiency": 0.05,
+            "uptake": 0.2,
+            "response": 100.0,
+            "time": 1e-3 * 50,
+            "mass_fraction": 0.5,
+        },
+    )
+    result.fromNebulisationEfficiency()
+
+    assert result.ionic_background == result.background / 100.0
+    assert result.number_concentration is not None
+    assert result.mass_concentration is not None
+
+    assert np.all(result.detections["mass"] == result.asMass(detections))
+    assert np.all(result.detections["size"] == result.asSize(detections))
