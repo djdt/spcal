6ff0cf976d1ec04b0a181d8c46c2d54c2aecf485
djdt <10266332+djdt@users.noreply.github.com>
2023-01-04 13:59:23 +1100

    combine functions, raw param for raw data

--- spcal/io/nu.py
+++ spcal/io/nu.py
@@ -1,14 +1,35 @@
 import json
 from pathlib import Path
-from typing import BinaryIO, Dict, List, Tuple
+from typing import Dict, Tuple, Union
 
 import numpy as np
-import numpy.lib.recfunctions as rfn
-
-from spcal.npdb import db
 
 
-def read_integ_binary(
+def get_masses_from_nu_data(
+    data: np.ndarray, cal_coef: Tuple[float, float], segment_delays: Dict[int, float]
+) -> np.ndarray:
+    """Converts Nu peak centers into masses.
+
+    Args:
+        data: from `read_integ_binary`
+        delays: array of delays from `get_delays_for_segments`
+        cal_coef: from run.info 'MassCalCoefficients'
+
+    Returns:
+        1d named array of signals
+    """
+
+    delays = np.zeros(max(segment_delays.keys()))
+    for k, v in segment_delays.items():
+        delays[k - 1] = v
+    delays = np.atleast_1d(delays[data["seg_number"] - 1])
+
+    masses = (data["result"]["center"] * 0.5) + delays[:, None]
+    # Convert from time to mass (sqrt(m/q) = a + t * b)
+    return (cal_coef[0] + masses * cal_coef[1]) ** 2
+
+
+def read_nu_integ_binary(
     path: Path,
     first_cyc_number: int | None = None,
     first_seg_number: int | None = None,
@@ -47,9 +68,22 @@ def read_integ_binary(
         return np.frombuffer(fp.read(), dtype=integ_dtype(num_results))
 
 
+def is_nu_directory(path: str | Path) -> bool:
+    """Checks path is directory containing a 'run.info' and 'integrated.index'"""
+    path = Path(path)
+    if not path.is_dir():
+        return False
+    if not path.joinpath("run.info").exists():
+        return False
+    if not path.joinpath("integrated.index").exists():
+        return False
+
+    return True
+
+
 def read_nu_directory(
-    path: str | Path, threads: int | None = 1
-) -> Tuple[np.ndarray, dict]:
+    path: str | Path, threads: int | None = 1, raw: bool = False
+) -> Union[Tuple[np.ndarray, dict], Tuple[np.ndarray, np.ndarray, dict]]:
     """Read the Nu Instruments raw data directory, retuning data and run info.
 
     Directory must contain 'run.info', 'integrated.index' and at least one '.integ'
@@ -66,16 +100,12 @@ def read_nu_directory(
         dict of parameters from run.info
     """
     path = Path(path)
-    run_path = path.joinpath("run.info")
-    if not run_path.exists():
-        raise FileNotFoundError("read_nu_directory: missing 'run.info'")
-    with run_path.open("r") as fp:
-        run_info = json.load(fp)
+    if not is_nu_directory(path):
+        raise ValueError("read_nu_directory: missing 'run.info' or 'integrated.index'")
 
-    integ_path = path.joinpath("integrated.index")
-    if not integ_path.exists():
-        raise FileNotFoundError("read_nu_directory: missing 'integrated.index'")
-    with integ_path.open("r") as fp:
+    with path.joinpath("run.info").open("r") as fp:
+        run_info = json.load(fp)
+    with path.joinpath("integrated.index").open("r") as fp:
         integ_index = json.load(fp)
 
     # Todo, single threaded is faster on uni PC, check others
@@ -84,7 +114,7 @@ def read_nu_directory(
     if threads == 1:
         data = np.concatenate(
             [
-                read_integ_binary(
+                read_nu_integ_binary(
                     path.joinpath(f"{idx['FileNum']}.integ"),
                     idx["FirstCycNum"],
                     idx["FirstSegNum"],
@@ -100,7 +130,7 @@ def read_nu_directory(
         with concurrent.futures.ProcessPoolExecutor(max_workers=threads) as ex:
             futures = [
                 ex.submit(
-                    read_integ_binary,
+                    read_nu_integ_binary,
                     path.joinpath(f"{idx['FileNum']}.integ"),
                     idx["FirstCycNum"],
                     idx["FirstSegNum"],
@@ -118,52 +148,14 @@ def read_nu_directory(
         order = np.argsort(data["acq_number"])
         data = data[order]
 
-    return data, run_info
+    if raw:
+        return data, run_info
 
+    segment_delays = {
+        s["Num"]: s["AcquisitionTriggerDelayNs"] for s in run_info["SegmentInfo"]
+    }
 
-def format_nu_integ_data(
-    data: np.ndarray, cal_coef: Tuple[float, float], segment_delays: Dict[int, float]
-) -> np.ndarray:
-    """Converts Nu data into a structured array of integrated signals.
-
-    Uses data and info from `read_integ_binary`. The returned array is a 1d named
-    array of signals, with each name being the symbol and isotope with the mass
-    closest to that of the integrated regions center, in the format Ag107.
-    Requires all data to have the same peak centers and segment number.
-
-    Args:
-        data: from `read_integ_binary`
-        cal_coef: from run.info 'MassCalCoefficients'
-        segment_delays: dict of segments 'Num' to 'AcquisitionTriggerDelayNs'
-
-    Returns:
-        1d named array of signals
-    """
-
-    def isotopes_for_masses(masses: np.ndarray) -> np.ndarray:
-        idx = np.argmin(np.abs(db["isotopes"]["Mass"] - masses[:, None]), axis=1)
-        return db["isotopes"][idx]
-
-    # Todo, we could calculate the actual times and only require the Symbol/Isotope to be the same.
-    if not np.all(data["result"]["center"] == data["result"]["center"][0]):
-        raise ValueError(
-            "format_nu_integ_data: unable to format, variable peak centers"
-        )
-    if not np.all(data["seg_number"] == data["seg_number"][0]):
-        raise ValueError("format_nu_integ_data: segments must all be the same")
-
-    # Masses are shared across data
-    masses = data["result"]["center"][0] * 0.5
-    masses += segment_delays[data["seg_number"][0]]
-
-    # Convert from time to mass (sqrt(m/q) = a + t * b)
-    masses = (cal_coef[0] + masses * cal_coef[1]) ** 2
-
-    dtype = np.dtype(
-        [
-            (f"{iso['Symbol']}{iso['Isotope']}", np.float32)
-            for iso in isotopes_for_masses(masses)
-        ]
+    masses = get_masses_from_nu_data(
+        data[0], run_info["MassCalCoefficients"], segment_delays
     )
-
-    return rfn.unstructured_to_structured(data["result"]["signal"], dtype=dtype)
+    return masses, data["result"]["signal"], run_info
