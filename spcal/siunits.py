4129d7069a3b4f7afbb952bc04b120c9552740e6
djdt <10266332+djdt@users.noreply.github.com>
2023-01-07 10:30:19 +1100

    move siunits to file

---
+++ spcal/siunits.py
@@ -0,0 +1,34 @@
+mass_units = {
+    "ag": 1e-21,
+    "fg": 1e-18,
+    "pg": 1e-15,
+    "ng": 1e-12,
+    "μg": 1e-9,
+    "g": 1e-3,
+    "kg": 1.0,
+}
+
+mass_concentration_units = {
+    "fg/L": 1e-18,
+    "pg/L": 1e-15,
+    "ng/L": 1e-12,
+    "μg/L": 1e-9,
+    "mg/L": 1e-6,
+    "g/L": 1e-3,
+    "kg/L": 1.0,
+}
+molar_concentration_units = {
+    "amol/L": 1e-18,
+    "fmol/L": 1e-15,
+    "pmol/L": 1e-12,
+    "nmol/L": 1e-9,
+    "μmol/L": 1e-6,
+    "mmol/L": 1e-3,
+    "mol/L": 1.0,
+}
+
+signal_units = {"counts": 1.0}
+
+size_units = {"nm": 1e-9, "μm": 1e-6, "mm": 1e-3, "m": 1.0}
+
+time_units = {"μs": 1e-6, "ms": 1e-3, "s": 1.0}
