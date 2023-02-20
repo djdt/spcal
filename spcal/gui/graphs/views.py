d2d124c13dd08484293e8b4e9117956c586c4cb1
djdt <10266332+djdt@users.noreply.github.com>
2023-02-21 08:44:54 +1100

    label alignment

--- spcal/gui/graphs/views.py
+++ spcal/gui/graphs/views.py
@@ -337,19 +337,13 @@ class PCAArrow(QtWidgets.QGraphicsPathItem):
         self.setPath(path)
 
         if name is not None:
-            pos = tr.map(QtCore.QPointF(0.0, -length))
             self.text = QtWidgets.QGraphicsSimpleTextItem(name, self)
             rect = QtGui.QFontMetrics(self.text.font()).boundingRect(self.text.text())
-            if angle_r > 3.0 / 2.0 * np.pi:
-                # pos = self.boundingRect().topLeft()
-                pos.setY(pos.y() - rect.height())
-            elif angle_r > np.pi:
-                pos = self.boundingRect().bottomLeft()
-            elif angle_r > np.pi / 2.0:
-                pos = self.boundingRect().bottomRight()
-            else:
-                # pos = self.boundingRect().topRight()
-                pos.setY(pos.y() - rect.height())
+            pos = tr.map(QtCore.QPointF(0.0, -(length)))
+            pos.setX(pos.x() - rect.width() / 2.0)
+            pos.setY(pos.y() - rect.height() / 2.0)
+            # if angle_r > np.pi:  # Left side of plot
+            #     pos.setX(pos.x() - rect.width())
 
             self.text.setPos(pos)
 
@@ -385,10 +379,10 @@ class PCAView(SinglePlotGraphicsView):
             assert len(feature_names) == v.shape[1]
 
             angles = np.arctan2(v[0], v[1])
-            for angle in np.linspace(0.0, 2.0 * np.pi, 10):
-                arrow = PCAArrow(angle, 100.0, name=str(angle * 180.0 / np.pi))
-            # for name, angle in zip(feature_names, angles):
-            #     arrow = PCAArrow(angle, 100.0, name=name)
+            # for angle in np.linspace(0.0, 2.0 * np.pi, 10):
+                # arrow = PCAArrow(angle, 100.0, name=f"{angle * 180.0 / np.pi:.18f}")
+            for name, angle in zip(feature_names, angles):
+                arrow = PCAArrow(angle, 100.0, name=name)
                 self.plot.addItem(arrow)
 
 
