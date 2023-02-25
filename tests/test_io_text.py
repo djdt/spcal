29e440cb0dda7682a75d13d1ee8f14e5bd863d51
djdt <10266332+djdt@users.noreply.github.com>
2023-02-25 16:04:30 +1100

    test is_text_file

--- tests/test_io_text.py
+++ tests/test_io_text.py
@@ -3,7 +3,13 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from spcal.io.text import read_single_particle_file
+from spcal.io.text import is_text_file, read_single_particle_file
+
+
+def test_io_is_text_file():
+    assert is_text_file(Path(__file__).parent.joinpath("data/text/text_normal.csv"))
+    assert not is_text_file(Path(__file__).parent.joinpath("data/text/text_normal.bad"))
+    assert not is_text_file(Path(__file__).parent.joinpath("data/text/"))
 
 
 def test_io_text_import():
