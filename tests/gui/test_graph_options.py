6d72a358d12db141cc2d867345a955606cbb5e93
djdt <10266332+djdt@users.noreply.github.com>
2023-03-07 17:16:39 +1100

    add test for options dialogs

---
+++ tests/gui/test_graph_options.py
@@ -0,0 +1,98 @@
+from PySide6 import QtCore
+from pytestqt.qtbot import QtBot
+
+from spcal.gui.dialogs.graphoptions import (
+    CompositionsOptionsDialog,
+    HistogramOptionsDialog,
+)
+
+
+def test_composition_options_dialog(qtbot: QtBot):
+    dlg = CompositionsOptionsDialog(distance=1.0, minimum_size=100.0)
+    qtbot.add_widget(dlg)
+    with qtbot.wait_exposed(dlg):
+        dlg.show()
+
+    assert dlg.lineedit_distance.text() == "100.0"
+    assert dlg.lineedit_size.text() == "100.0"
+
+    # No change, no signal
+    with qtbot.assert_not_emitted(dlg.distanceChanged, wait=100):
+        dlg.apply()
+
+    dlg.lineedit_distance.setText("10.0")
+    dlg.lineedit_size.setText("1%")
+
+    with qtbot.wait_signals(
+        [dlg.distanceChanged, dlg.minimumSizeChanged],
+        timeout=100,
+        check_params_cbs=[lambda d: d == 0.1, lambda s: s == "1%"],
+    ):
+        dlg.apply()
+
+    # Reset to default values
+    dlg.reset()
+
+    with qtbot.wait_signals(
+        [dlg.distanceChanged, dlg.minimumSizeChanged],
+        timeout=100,
+        check_params_cbs=[lambda d: d == 0.03, lambda s: s == "5%"],
+    ):
+        dlg.apply()
+    dlg.accept()
+
+
+def test_histogram_options_dialog(qtbot: QtBot):
+    dlg = HistogramOptionsDialog(
+        fit="log normal",
+        bin_widths={
+            "signal": None,
+            "mass": None,
+            "size": None,
+            "cell_concentration": 1e-9,
+        },
+    )
+    qtbot.add_widget(dlg)
+    with qtbot.wait_exposed(dlg):
+        dlg.show()
+
+    # No change, no signal
+    with qtbot.assert_not_emitted(dlg.fitChanged, wait=100):
+        dlg.apply()
+
+    qtbot.mouseClick(dlg.radio_fit_norm, QtCore.Qt.MouseButton.LeftButton)
+    dlg.width_signal.setBaseValue(100.0)
+    dlg.width_mass.setBaseValue(1e-19)
+    dlg.width_size.setBaseValue(1e-9)
+
+    def check_bin_widths(widths: dict) -> bool:
+        if widths["signal"] != 100.0:
+            return False
+        if widths["mass"] != 1e-19:
+            return False
+        if widths["size"] != 1e-9:
+            return False
+        if widths["cell_concentration"] != 1e-9:
+            return False
+        return True
+
+    with qtbot.wait_signals(
+        [dlg.fitChanged, dlg.binWidthsChanged],
+        timeout=100,
+        check_params_cbs=[lambda f: f == "normal", check_bin_widths],
+    ):
+        dlg.apply()
+
+    # Reset to default values
+    dlg.reset()
+
+    with qtbot.wait_signals(
+        [dlg.fitChanged, dlg.binWidthsChanged],
+        timeout=100,
+        check_params_cbs=[
+            lambda f: f == "log normal",
+            lambda d: all(x is None for x in d.values()),
+        ],
+    ):
+        dlg.apply()
+    dlg.accept()
