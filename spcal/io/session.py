19b9477781e35c0b96234bdcf22964091715d24a
djdt <10266332+djdt@users.noreply.github.com>
2023-03-21 18:27:16 +1100

    fixup the session export

--- spcal/io/session.py
+++ spcal/io/session.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import h5py
 
 from spcal import __version__
-from spcal.gui.inputs import ReferenceWidget, SampleWidget
+from spcal.gui.inputs import InputWidget, ReferenceWidget, SampleWidget
 from spcal.gui.options import OptionsWidget
 
 
@@ -45,23 +45,24 @@ def saveSession(
         for key, val in options.state().items():
             options_group.attrs[key] = val
 
-        for key, input in zip(["sample", "reference"], [sample, reference]):
+        input: InputWidget
+        for input_key, input in zip(["sample", "reference"], [sample, reference]):  # type: ignore
             if input.responses.dtype.names is not None:
-                h5.create_group(key)
-                dset = h5[key].create_dataset(
+                input_group = h5.create_group(input_key)
+                dset = input_group.create_dataset(
                     "data", data=input.responses, compression="gzip"
                 )
                 dset.attrs["trim"] = input.trimRegion("")
 
-                group = h5[key].create_group("import options")
+                import_group = input_group.create_group("import options")
                 for key, val in sanitiseImportOptions(input.import_options).items():
-                    group.attrs[key] = val
+                    import_group.attrs[key] = val
 
-                element_group = h5[key].create_group("elements")
+                element_group = input_group.create_group("elements")
                 for name in input.responses.dtype.names:
-                    group = element_group.create_group(name)
+                    name_group = element_group.create_group(name)
                     for key, val in input.io[name].state().items():
-                        group.attrs[key] = val
+                        name_group.attrs[key] = val
 
         # Results - filters
 
@@ -75,7 +76,8 @@ def restoreSession(
 
         options.setState(h5["options"].attrs)
 
-        for key, input in zip(["sample", "reference"], [sample, reference]):
+        input: InputWidget
+        for key, input in zip(["sample", "reference"], [sample, reference]):  # type: ignore
             if key in h5:
                 data = h5[key]["data"][:]
                 import_options = restoreImportOptions(h5[key]["import options"].attrs)
