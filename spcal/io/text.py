1a968fed0d06e16556900530088cde5cc5e87c62
djdt <10266332+djdt@users.noreply.github.com>
2023-03-31 13:15:08 +1100

    replace spaces in dtype names

--- spcal/io/text.py
+++ spcal/io/text.py
@@ -87,6 +87,7 @@ def read_single_particle_file(
                 dtype=dtype,
                 max_rows=max_rows,
                 converters={c: lambda s: np.float32(s or 0) for c in columns},
+                replace_space="_",
                 invalid_raise=False,
                 loose=True,
             )
@@ -99,6 +100,8 @@ def read_single_particle_file(
 
     if new_names is not None:
         data.dtype.names = new_names
+
+    data.dtype.names = tuple(name.replace(" ", "_") for name in data.dtype.names)
 
     if convert_cps is not None:
         for name in data.dtype.names:
