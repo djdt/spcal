84b3730166f25784bb1c1150c7072a6098f58659
djdt <10266332+djdt@users.noreply.github.com>
2023-04-12 19:15:31 +1000

    changes to limits altered detection number slightly

--- tests/gui/test_sample_with_reference.py
+++ tests/gui/test_sample_with_reference.py
@@ -118,7 +118,7 @@ def test_sample_with_reference(qtbot: QtBot):
     assert np.isclose(window.results.io["Au"].mean.baseValue(), 2091, atol=1)
     assert np.isclose(window.results.io["Au"].median.baseValue(), 2034, atol=1)
     assert np.isclose(window.results.io["Au"].lod.baseValue(), limit, atol=0.1)
-    assert np.isclose(window.results.io["Au"].number.baseValue(), 7.945e8, rtol=1e-4)
+    assert np.isclose(window.results.io["Au"].number.baseValue(), 7.946e8, rtol=1e-4)
     assert np.isclose(window.results.io["Au"].conc.baseValue(), 8.788e-10, rtol=1e-4)
     assert np.isclose(
         window.results.io["Au"].background.baseValue(), 1.906e-11, rtol=1e-4
