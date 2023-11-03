11f7b95e8c98ac5cdffb30c9a719f570e44e5c8e
djdt <10266332+djdt@users.noreply.github.com>
2023-11-03 16:23:36 +1100

    include max_deviation

--- spcal/calc.py
+++ spcal/calc.py
@@ -101,7 +101,7 @@ def is_integer_or_near(x: np.ndarray, max_deviation: float = 1e-3) -> np.ndarray
     """
     if max_deviation < 0.0 or max_deviation >= 1.0:
         raise ValueError("'max_deviation' must be in the range [0-1).")
-    return np.abs(x - np.round(x)) < max_deviation
+    return np.abs(x - np.round(x)) <= max_deviation
 
 
 def otsu(x: np.ndarray, remove_nan: bool = False, nbins: str | int = "fd") -> float:
