b4d326b19f942a33b1d74ee5db236c055b371f20
djdt <10266332+djdt@users.noreply.github.com>
2024-02-15 16:32:00 +1100

    add cal test for nu

--- tests/test_io_nu.py
+++ tests/test_io_nu.py
@@ -3,11 +3,20 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from spcal.io.nu import (  # get_masses_from_nu_data,
+from spcal.io.nu import (
+    get_dwelltime_from_info,
     is_nu_directory,
+    is_nu_run_info_file,
     read_nu_directory,
     select_nu_signals,
+    single_ion_distribution,
 )
+
+
+def test_is_nu_run_info_file():
+    path = Path(__file__).parent.joinpath("data/nu")
+    assert not is_nu_run_info_file(path)
+    assert is_nu_run_info_file(path.joinpath("run.info"))
 
 
 def test_is_nu_dir():
@@ -19,7 +28,7 @@ def test_is_nu_dir():
 
 def test_io_nu_import():
     path = Path(__file__).parent.joinpath("data/nu")
-    masses, signals, info = read_nu_directory(path)
+    masses, signals, info = read_nu_directory(path, cycle=1, segment=1)
     assert masses.size == 194
     assert signals.shape == (15474, 194)
     signals = signals[np.all(~np.isnan(signals), axis=1), :]
@@ -48,6 +57,21 @@ def test_io_nu_import():
     # fmt: on
     assert np.all(signals[:, 193] == 0.0)  # uranium?
 
+    assert np.isclose(get_dwelltime_from_info(info), 8.289e-5)
+
+
+def test_io_nu_import_max_files():
+    path = Path(__file__).parent.joinpath("data/nu")
+    masses, signals, info = read_nu_directory(
+        path, max_integ_files=1, cycle=1, segment=1
+    )
+    assert masses.size == 194
+    assert signals.shape == (10, 194)
+
+
+# need data, todo
+# def test_io_nu_import_autoblank():
+
 
 def test_select_nu_signals():
     masses = np.array([1.0, 2.0, 10.0])
@@ -60,3 +84,11 @@ def test_select_nu_signals():
 
     with pytest.raises(ValueError):
         select_nu_signals(masses, signals, {"a": 2.0, "b": 9.95}, max_mass_diff=0.01)
+
+
+def test_single_ion_distribution():
+    path = Path(__file__).parent.joinpath("data/nu/cal")
+    masses, signals, info = read_nu_directory(path, cycle=1, segment=1)
+    y = single_ion_distribution(signals)
+    # not sure how to test this?
+    assert y[np.argmax(y[:, 0]), 1] == 1.0
