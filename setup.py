7fdf3f8bfecf9dfd72de84b5fea3d11fbde65a89
djdt <10266332+djdt@users.noreply.github.com>
2024-02-22 17:44:16 +1100

    include and link openmp

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ spcalext = Extension(
     sources=["src/spcalext.c"],
     include_dirs=[numpy.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    extra_compile_args=["-fopenmp"],
+    extra_link_args=["-fopenmp"],
 )
 
 setup(
