725ee78af585162f65428b6dd5d95ba50719f8e5
djdt <10266332+djdt@users.noreply.github.com>
2024-02-23 12:15:34 +1100

    parallel mergesort

--- src/spcalext.c
+++ src/spcalext.c
@@ -133,6 +133,45 @@ void label(int *Z, int n) {
   free(sizes);
 }
 
+void _merge_argsort(struct argsort *x, int n, struct argsort *t) {
+  int i = 0, j = n / 2, ti = 0;
+
+  while (i < n / 2 && j < n) {
+    if (x[i].value < x[j].value) {
+      t[ti++] = x[i++];
+    } else {
+      t[ti++] = x[j++];
+    }
+  }
+  while (i < n / 2) {
+    t[ti++] = x[i++];
+  }
+  while (j < n) {
+    t[ti++] = x[j++];
+  }
+  memcpy(x, t, n * sizeof(struct argsort));
+}
+
+void _mergesort_argsort_rec(struct argsort *x, int n, struct argsort *t) {
+  if (n < 2)
+    return;
+#pragma omp task shared(x) if (n > 1000)
+  _mergesort_argsort_rec(x, n / 2, t);
+#pragma omp task shared(x) if (n > 1000)
+  _mergesort_argsort_rec(x + n / 2, n - n / 2, t + n / 2);
+#pragma omp taskwait
+  _merge_argsort(x, n, t);
+}
+void mergesort_argsort(struct argsort *x, int n) {
+  struct argsort *t = malloc(n * sizeof(struct argsort));
+#pragma omp parallel
+  {
+#pragma omp single
+    _mergesort_argsort_rec(x, n, t);
+  }
+  free(t);
+}
+
 static PyObject *mst_linkage(PyObject *self, PyObject *args) {
   PyObject *in;
   PyArrayObject *PDarray;
@@ -194,7 +233,7 @@ static PyObject *mst_linkage(PyObject *self, PyObject *args) {
   Py_DECREF(PDarray);
 
   // Sort
-  qsort(Z3, n - 1, sizeof(Z3[0]), argsort_cmp);
+  mergesort_argsort(Z3, n - 1);
 
   PyArrayObject *Zarray, *ZDarray;
   npy_intp Zdims[] = {n - 1, 3};
