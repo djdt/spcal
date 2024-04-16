0d346a1efdfa1de72847c83c0b158f1263048e29
djdt <10266332+djdt@users.noreply.github.com>
2024-04-16 16:19:00 +1000

    fix error when massresponse for wrong name is used

--- spcal/gui/results.py
+++ spcal/gui/results.py
@@ -924,7 +924,7 @@ class ResultsWidget(QtWidgets.QWidget):
                     inputs["efficiency"] = self.options.efficiency.value()
                 elif method == "Reference Particle":
                     inputs["efficiency"] = self.reference.getEfficiency(name)
-                elif method == "Mass Response":
+                elif method == "Mass Response" and name in self.reference.io:
                     inputs["mass_response"] = self.reference.io[
                         name
                     ].massresponse.baseValue()
