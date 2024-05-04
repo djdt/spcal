aab3eddaf6b547222a7972619bb2ad048c2e33f9
djdt <10266332+djdt@users.noreply.github.com>
2024-05-04 22:20:09 +1000

    no statis

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,4 +27,4 @@ templates_path = ["_templates"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
