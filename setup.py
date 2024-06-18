27ffbcc691c6b7d4e05903e1b61f378bfd46b970
djdt <10266332+djdt@users.noreply.github.com>
2024-06-18 18:08:29 +1000

    msvc uses different compile arg syntax

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 import numpy
 from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import new_compiler
+
+compiler = new_compiler().compiler_type
+
+if compiler == "msvc":
+    extra_compile_args = ["/openmp"]
+    extra_link_args = ["/openmp"]
+else:
+    extra_compile_args = ["-fopenmp"]
+    extra_link_args = ["-fopenmp"]
 
 spcalext = Extension(
     "spcal.lib.spcalext",
     sources=["src/sort.c", "src/spcalext.c"],
     include_dirs=["include", numpy.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_2_0_API_VERSION")],
-    extra_compile_args=["-fopenmp"],
-    extra_link_args=["-fopenmp"],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
