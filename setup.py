42d4d317fcf08b0af2b430eab04a8645d2016ba5
djdt <10266332+djdt@users.noreply.github.com>
2024-06-19 11:02:11 +1000

    give correct compiler option to msvc

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 import numpy
 from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import new_compiler
+
+compiler = new_compiler().compiler_type
+
+if compiler == "msvc":
+    extra_compile_args = ["/openmp:llvm"]
+    extra_link_args = ["/openmp:llvm"]
+else:
+    extra_compile_args = ["-fopenmp"]
+    extra_link_args = ["-fopenmp"]
 
 spcalext = Extension(
     "spcal.lib.spcalext",
