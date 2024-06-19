582cbe14f728df5897891dcf4c3a9300f2275f16
djdt <10266332+djdt@users.noreply.github.com>
2024-06-19 11:07:05 +1000

    use args

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ spcalext = Extension(
     sources=["src/sort.c", "src/spcalext.c"],
     include_dirs=["include", numpy.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-    extra_compile_args=["-fopenmp"],
-    extra_link_args=["-fopenmp"],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
