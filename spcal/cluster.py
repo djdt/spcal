e5bd28d7464bf75630f9811b592fb899cfad35b2
djdt <10266332+djdt@users.noreply.github.com>
2024-06-19 16:06:43 +1000

    prevent crash when 0 size data clustered

--- spcal/cluster.py
+++ spcal/cluster.py
@@ -40,6 +40,8 @@ def agglomerative_cluster(X: np.ndarray, max_dist: float) -> np.ndarray:
     Returns:
         cluster indicies
     """
+    if X.size == 0:
+        return np.array([], dtype=int)
     dists = pairwise_euclidean(X)
     Z, ZD = mst_linkage(dists, X.shape[0])
     return cluster_by_distance(Z, ZD, max_dist) - 1
