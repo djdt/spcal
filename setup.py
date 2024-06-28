ffcca1a182dd93d5472cc16b523ddeb1e61c2e83
djdt <10266332+djdt@users.noreply.github.com>
2024-06-28 19:55:09 +1000

    specific compile time options for msvc / gcc

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import find_packages, setup
+from setuptools.command.build_ext import new_compiler
+
+compiler = new_compiler().compiler_type
+
+if compiler == "msvc":
+    cxx_std = "latest"
+    extra_link_args = []
+else:
+    cxx_std = "23"
+    extra_link_args = ["-ltbb"]
 
 spcalext = Pybind11Extension(
     "spcal.lib.spcalext",
     sources=["src/spcalext.cpp"],
-    cxx_std=23,
-    extra_link_args=["-ltbb"],
+    cxx_std=cxx_std,
+    extra_link_args=extra_link_args,
 )
 
 setup(
     packages=find_packages(include=["spcal", "spcal.*"]),
+    cmd_class={"build_ext": build_ext},
     ext_modules=[spcalext],
 )
