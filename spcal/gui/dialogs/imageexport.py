41100203df3ccbee90c7902d4b166addeb60a320
djdt <10266332+djdt@users.noreply.github.com>
2024-12-19 13:53:42 +1100

    copy the whole chart

--- spcal/gui/dialogs/imageexport.py
+++ spcal/gui/dialogs/imageexport.py
@@ -1,3 +1,4 @@
+import pyqtgraph
 from PySide6 import QtCore, QtGui, QtWidgets
 
 from spcal.gui.graphs.base import SinglePlotGraphicsView
@@ -8,7 +9,8 @@ class ImageExportDialog(QtWidgets.QDialog):
         self, graph: SinglePlotGraphicsView, parent: QtWidgets.QWidget | None = None
     ):
         super().__init__(parent)
-        self.graph = graph
+        self.graph = self.createGraphCopy(graph)
+        # self.graph = graph
         size = graph.viewport().rect()
 
         self.spinbox_size_x = QtWidgets.QSpinBox()
@@ -41,26 +43,65 @@ class ImageExportDialog(QtWidgets.QDialog):
         layout_form.addRow("DPI:", self.spinbox_dpi)
 
         layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.graph)
         layout.addLayout(layout_form)
         layout.addWidget(self.button_box)
         self.setLayout(layout)
 
+    def createGraphCopy(self, graph: SinglePlotGraphicsView) -> SinglePlotGraphicsView:
+        copy = SinglePlotGraphicsView(
+            graph.plot.titleLabel.text,
+            graph.xaxis.labelText,
+            graph.yaxis.labelText,
+            graph.xaxis.labelUnits,
+            graph.yaxis.labelUnits,
+        )
+        for item in graph.plot.items:
+            if isinstance(item, pyqtgraph.PlotCurveItem):
+                pen = item.opts["pen"]
+                # resize pen
+                curve = pyqtgraph.PlotCurveItem(
+                    x=item.xData,
+                    y=item.yData,
+                    pen=pen,
+                    connect="all",
+                    skipFiniteCheck=True,
+                )
+                copy.plot.addItem(curve)
+            elif isinstance(item, pyqtgraph.ScatterPlotItem):
+                brush = item.opts["brush"]
+                scatter = pyqtgraph.ScatterPlotItem(
+                    x=item.data["x"],
+                    y=item.data["y"],
+                    size=item.opts["size"],
+                    symbol=item.opts["symbol"],
+                    pen=None,
+                    brush=brush,
+                )
+                copy.plot.addItem(scatter)
+
+        limits = graph.plot.vb.state["limits"]
+        copy.plot.vb.setLimits(
+            xMin=limits["xLimits"][0],
+            xMax=limits["xLimits"][1],
+            yMin=limits["yLimits"][0],
+            yMax=limits["yLimits"][1],
+        )
+        view_range = graph.plot.vb.state["viewRange"]
+        copy.plot.vb.setRange(xRange=view_range[0], yRange=view_range[1])
+        return copy
+
     def accept(self) -> None:
-        self.prepareForRender()
-        self.timer = QtCore.QTimer()
-        self.timer.setSingleShot(True)
-        self.timer.timeout.connect(self.render)
-        self.timer.start(100)
+        self.render()
+        # self.timer = QtCore.QTimer()
+        # self.timer.setSingleShot(True)
+        # self.timer.timeout.connect(self.render)
+        # self.timer.start(100)
+        super().accept()
 
     def prepareForRender(self) -> None:
         self.original_size = self.graph.size()
         self.original_font = QtGui.QFont(self.graph.font)
-        self.image = QtGui.QImage(
-            self.spinbox_size_x.value(),
-            self.spinbox_size_y.value(),
-            QtGui.QImage.Format.Format_ARGB32,
-        )
-        self.image.fill(QtGui.QColor(0, 0, 0, 0))
 
         resized_font = QtGui.QFont(self.original_font)
         resized_font.setPointSizeF(
@@ -74,23 +115,32 @@ class ImageExportDialog(QtWidgets.QDialog):
         self.graph.setFont(self.original_font)
 
     def render(self):
-        painter = QtGui.QPainter(self.image)
+        image = QtGui.QImage(
+            self.spinbox_size_x.value(),
+            self.spinbox_size_y.value(),
+            QtGui.QImage.Format.Format_ARGB32,
+        )
+        image.fill(QtGui.QColor(0, 0, 0, 0))
+
+        painter = QtGui.QPainter(image)
         painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
         painter.setRenderHint(QtGui.QPainter.RenderHint.TextAntialiasing, True)
 
-        self.graph.scene().prepareForPaint()
-        self.graph.scene().render(
+        graph = self.createGraphCopy(self.graph)
+        graph.resize(image.size())
+        graph.show()
+
+        graph.scene().prepareForPaint()
+        graph.scene().render(
             painter,
-            QtCore.QRectF(self.image.rect()),
-            self.graph.viewRect(),
+            QtCore.QRectF(image.rect()),
+            graph.viewRect(),
             QtCore.Qt.AspectRatioMode.KeepAspectRatio,
         )
         painter.end()
-        self.image.save("/home/tom/Downloads/out.png")
+        image.save("/home/tom/Downloads/out.png")
 
-        self.post_timer = QtCore.QTimer()
-        self.post_timer.setSingleShot(True)
-        self.post_timer.timeout.connect(self.postRender)
-        self.post_timer.start(100)
-
-        super().accept()
+        # self.post_timer = QtCore.QTimer()
+        # self.post_timer.setSingleShot(True)
+        # self.post_timer.timeout.connect(self.postRender)
+        # self.post_timer.start(100)
