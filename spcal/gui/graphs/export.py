d8d65e1a7d225ddcec0607b02b2ca1743e1419bb
djdt <10266332+djdt@users.noreply.github.com>
2024-12-23 10:29:52 +1100

    move in code for start of hist export

--- spcal/gui/graphs/export.py
+++ spcal/gui/graphs/export.py
@@ -48,6 +48,58 @@ def draw_particle_view(
             )
     return graph
 
-# def draw_histogram_view(
-#     results: dict[str, SPCalResult],
-#         )
+def draw_histogram_view(
+    results: dict[str, SPCalResult],
+        )
+
+        for i, (name, data) in enumerate(graph_data.items()):
+            color = self.colorForName(name)
+            max_bin = np.percentile(
+                data[graph_idx[name]], self.graph_options["histogram"]["percentile"]
+            )
+            bins = np.arange(
+                data.min() * modifier, max_bin * modifier + bin_width, bin_width
+            )
+            if bins.size == 1:
+                bins = np.array([bins[0], max_bin])
+            bins -= bins[0] % bin_width  # align bins
+            if self.graph_options["histogram"]["mode"] == "overlay":
+                width = 1.0 / len(graph_data)
+                offset = i * width
+            elif self.graph_options["histogram"]["mode"] == "single":
+                width = 1.0
+                offset = 0.0
+            else:
+                raise ValueError("drawGraphHist: invalid draw mode")
+
+            lod = self.results[name].convertTo(
+                self.results[name].limits.detection_limit, key
+            )
+
+            non_zero = np.flatnonzero(data)
+
+            limits = {"mean": np.mean(data[graph_idx[name]]) * modifier}
+            if isinstance(lod, np.ndarray):
+                limits["LOD (mean)"] = np.nanmean(lod) * modifier
+            else:
+                limits["LOD"] = lod * modifier
+
+            # Auto SI prefix does not work with squared (or cubed) units
+            self.graph_hist.xaxis.enableAutoSIPrefix(mode not in ["Signal", "Volume"])
+
+            self.graph_hist.xaxis.setLabel(text=label, units=unit)
+            self.graph_hist.draw(
+                data[graph_idx[name]] * modifier,
+                filtered_data=data[np.setdiff1d(non_zero, idx, assume_unique=True)]
+                * modifier,
+                bins=bins,
+                bar_width=width,
+                bar_offset=offset,
+                brush=QtGui.QBrush(color),
+                name=name,
+                draw_fit=self.graph_options["histogram"]["fit"],
+                fit_visible=self.graph_options["histogram"]["mode"] == "single",
+                draw_limits=limits,
+                limits_visible=self.graph_options["histogram"]["mode"] == "single",
+                draw_filtered=self.graph_options["histogram"]["draw filtered"],
+            )
