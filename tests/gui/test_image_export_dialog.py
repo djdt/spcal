5ee367433d300b0307360ae94282403d8b0378e1
djdt <10266332+djdt@users.noreply.github.com>
2025-01-07 13:33:57 +1100

    test accept

--- tests/gui/test_image_export_dialog.py
+++ tests/gui/test_image_export_dialog.py
@@ -1,18 +1,12 @@
-from pathlib import Path
 from PySide6 import QtCore
-
 from pytestqt.qtbot import QtBot
 
 from spcal.gui.dialogs.imageexport import ImageExportDialog
 
 
 def test_image_export_dialog(qtbot: QtBot):
-    path = Path("/home/test/fake_path.png")
     options = {"test option 1": False, "test option 2": False, "test option 3": True}
-    dlg = ImageExportDialog(
-        path,
-        options=options,
-    )
+    dlg = ImageExportDialog(options=options)
     qtbot.add_widget(dlg)
     with qtbot.wait_exposed(dlg):
         dlg.show()
@@ -26,17 +20,20 @@ def test_image_export_dialog(qtbot: QtBot):
     dlg.spinbox_size_x.setValue(400)
     dlg.spinbox_size_y.setValue(200)
 
-    # def check_settings(size: QtCore.QSize, dpi: float, options: dict[str,bool]) -> bool:
-    #     if size.width() != 400 or size.height() != 200:
-    #         return False
-    #     if dpi != 100.0:
-    #         return False
-    #     if options["test option 1"]:
-    #         return False
-    #     if not options["test option 2"]:
-    #         return False
-    #     if not options["test option 3"]:
-    #         return False
-    #     return True
+    def check_settings(
+        size: QtCore.QSize, dpi: float, options: dict[str, bool]
+    ) -> bool:
+        if size.width() != 400 or size.height() != 200:
+            return False
+        if dpi != 100.0:
+            return False
+        if options["test option 1"]:
+            return False
+        if not options["test option 2"]:
+            return False
+        if not options["test option 3"]:
+            return False
+        return True
 
-    # with qtbot.wait_signal(dlg.exportSettingsSelected, check_params_cb=check_settings):
+    with qtbot.wait_signal(dlg.exportSettingsSelected, check_params_cb=check_settings):
+        dlg.accept()
