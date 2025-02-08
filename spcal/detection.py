efe79571784b77556973bfe66f3d082a7dbc35be
djdt <10266332+djdt@users.noreply.github.com>
2025-02-08 14:07:38 +1100

    allow equal in local maxima

--- spcal/detection.py
+++ spcal/detection.py
@@ -57,7 +57,7 @@ def accumulate_detections(
 
     def local_maxima(x: np.ndarray):
         return np.logical_and(
-            np.r_[False, x[1:] > x[:-1]], np.r_[x[:-1] > x[1:], False]
+            np.r_[False, x[1:] >= x[:-1]], np.r_[x[:-1] >= x[1:], False]
         )
 
     if np.any(limit_accumulation > limit_detection):
