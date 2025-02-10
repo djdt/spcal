b3929dced1ff1b453f23ff61446c78e2e117fe3d
djdt <10266332+djdt@users.noreply.github.com>
2025-02-10 11:40:04 +1100

    add test for dist

--- tests/test_ext_dists.py
+++ tests/test_ext_dists.py
@@ -1,55 +1,11 @@
-import numpy as np
+from scipy.stats import poisson
 
-from spcal.lib.spcalext.clusterings import (
-    cluster_by_distance,
-    mst_linkage,
-    pairwise_euclidean,
-)
-from spcal.lib.spcalext.detection import (
-    cluster_by_distance,
-    mst_linkage,
-    pairwise_euclidean,
-)
+from spcal.lib.spcalext import dists
 
 
-def test_pairwise_euclidean():
-    a = np.arange(4.0).reshape(4, 1)
-    assert np.all(pairwise_euclidean(a) == [1.00, 2.0, 3.0, 1.0, 2.0, 1.0])
-    a = np.ones((9, 3))
-    assert np.all(pairwise_euclidean(a) == 0.0)
-    a = np.arange(16.0).reshape(4, 4)
-    assert np.all(pairwise_euclidean(a) == [8.0, 16.0, 24.0, 8.0, 16.0, 8.0])
-
-
-def test_mst_linkage():
-    a = np.array([0.1, 1.0, 0.2, 1.2, 3.3, 2.3, 0.7, 0.75]).reshape(8, 1)
-    dists = pairwise_euclidean(a)
-    Z, ZD = mst_linkage(dists, 8)
-
-    assert np.allclose(ZD, [0.05, 0.1, 0.2, 0.25, 0.5, 1.0, 1.1])
-    assert np.all(Z[0] == [6, 7])  # 0.05
-    assert np.all(Z[1] == [0, 2])  # 0.1
-    assert np.all(Z[2] == [1, 3])  # 0.2
-    assert np.all(Z[3] == [8, 10])
-    assert np.all(Z[4] == [9, 11])
-    assert np.all(Z[5] == [4, 5])
-    assert np.all(Z[6] == [12, 13])
-
-
-def test_cluster_by_distance():
-    a = np.array([0.1, 0.2, 0.3, 0.42, 0.5, 0.61, 0.72, 0.83]).reshape(8, 1)
-    dists = pairwise_euclidean(a)
-    Z, ZD = mst_linkage(dists, 8)
-    T = cluster_by_distance(Z, ZD, 0.05)
-    assert np.unique(T).size == 8  # all single clusters
-    T = cluster_by_distance(Z, ZD, 0.1)
-    assert np.all(T == [1, 1, 1, 2, 2, 3, 4, 5])
-    T = cluster_by_distance(Z, ZD, 0.2)
-    assert np.all(T == [1, 1, 1, 1, 1, 1, 1, 1])
-
-
-def test_maxima():
-    a = np.arange(10.0)
-    b = np.array([[1, 3], [2, 4], [3, 7], [9, 9]])
-    m = maxima(a, b)
-    assert np.all(m == [2, 3, 6, 9])
+def test_poisson_quantile():
+    for q in [0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999]:
+        for lam in [0.01, 1.0, 10.0, 100.0]:
+            a = dists.poisson_quantile(q, lam)
+            b = poisson.ppf(q, lam)
+            assert a == b
