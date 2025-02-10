775e5ab1796719050e08f31ed40153b3fe4e566f
djdt <10266332+djdt@users.noreply.github.com>
2025-02-10 14:30:53 +1100

    use actuall poisson data

--- tests/test_peak_integration.py
+++ tests/test_peak_integration.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from spcal import detection, poisson
@@ -9,32 +10,39 @@ def test_noise_level():
     """Tests detections / integrations staty the same when noise levels rise.
     Going further beyond the limit of crit will start to integrate noise.
     """
-    data = np.load(Path(__file__).parent.joinpath("data/tofwerk_auag.npz"))
-    for name in ["107Ag", "197Au"]:
-        x = data[name]
-        ub = np.mean(x)
-        yc, _ = poisson.formula_c(ub, alpha=0.001)
-        detections, _, _ = detection.accumulate_detections(
-            x, ub, yc + ub, integrate=True
+    data = np.load(Path(__file__).parent.joinpath("data/agilent_au_data.npz"))
+    x = data["au50nm"]
+    ub = np.mean(x)
+    yc, _ = poisson.formula_c(ub, alpha=0.001)
+    detections, _, _ = detection.accumulate_detections(
+        x, ub, yc + ub, integrate=True
+    )
+
+    truth = detections.size
+    truth_mean = detections.mean()
+
+    np.random.seed(14234)
+
+    for lam in np.linspace(ub, yc, 5, endpoint=True):
+        noise = np.random.poisson(lam=lam, size=x.size)
+
+        noise_x = x + noise
+
+        noise_ub = np.mean(noise_x)
+        noise_yc, _ = poisson.formula_c(noise_ub, alpha=0.001)
+        detections, _, regions = detection.accumulate_detections(
+            noise_x,
+            noise_ub,
+            noise_yc + noise_ub,
+            integrate=True,
         )
 
-        truth = detections.size
-        truth_mean = detections.mean()
+        plt.plot(noise_x[:300], c="k")
+        plt.scatter(regions.flat, np.ones(regions.size))
+        plt.xlim(0, 300)
+        plt.show()
 
-        np.random.seed(142893)
-
-        for lam in np.linspace(ub, yc, 5, endpoint=True):
-            noise = np.random.poisson(lam=lam, size=x.size)
-
-            noise_x = x + noise
-
-            noise_ub = np.mean(noise_x)
-            noise_yc, _ = poisson.formula_c(ub, alpha=0.001)
-            detections, _, _ = detection.accumulate_detections(
-                noise_x, noise_ub, noise_yc + noise_ub, integrate=True
-            )
-
-            # Less than 1% change in number of detections
-            assert abs(truth - detections.size) / truth < 0.01
-            # Less than 5% change in mean detected area
-            assert abs(truth_mean - detections.mean()) / truth_mean < 0.05
+        # Less than 1% change in number of detections
+        assert abs(truth - detections.size) / truth < 0.01
+        # Less than 5% change in mean detected area
+        assert abs(truth_mean - detections.mean()) / truth_mean < 0.05
