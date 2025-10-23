366e95b9c70563a86ce6c5655774f1d67a0a1ab7
djdt <10266332+djdt@users.noreply.github.com>
2025-10-23 09:53:26 +0200

    drawing

--- spcal/gui/graphs/particle.py
+++ spcal/gui/graphs/particle.py
@@ -2,9 +2,11 @@ import numpy as np
 import pyqtgraph
 from PySide6 import QtCore, QtGui, QtWidgets
 
+from spcal.detection import detection_maxima
 from spcal.gui.graphs.base import SinglePlotGraphicsView
 from spcal.gui.graphs.legends import MultipleItemSampleProxy
 from spcal.gui.util import create_action
+from spcal.processing import SPCalProcessingResult
 
 
 class ParticleView(SinglePlotGraphicsView):
@@ -29,22 +31,26 @@ class ParticleView(SinglePlotGraphicsView):
         self.xaxis.setScale(xscale)
         self.xaxis.enableAutoSIPrefix(False)
 
-        self.raw_signals: dict[str, np.ndarray] = {}  # for export
+        # self.raw_signals: dict[str, np.ndarray] = {}  # for export
+        self.result_items: dict[
+            str, tuple[pyqtgraph.PlotCurveItem, pyqtgraph.ScatterPlotItem]
+        ] = {}
 
-        self.plot.setLimits(xMin=0.0, xMax=1.0, yMin=0.0)
+        assert self.plot.vb is not None
+        self.plot.vb.setLimits(xMin=0.0, xMax=1.0, yMin=0.0)
         self.setAutoScaleY(True)
 
         self.legend_items: dict[str, MultipleItemSampleProxy] = {}
         self.limit_items: list[pyqtgraph.PlotCurveItem] = []
 
-        region_pen = QtGui.QPen(QtCore.Qt.red, 1.0)
+        region_pen = QtGui.QPen(QtCore.Qt.GlobalColor.red, 1.0)
         region_pen.setCosmetic(True)
 
         self.region = pyqtgraph.LinearRegionItem(
             pen="grey",
             hoverPen="red",
-            brush=QtGui.QBrush(QtCore.Qt.NoBrush),
-            hoverBrush=QtGui.QBrush(QtCore.Qt.NoBrush),
+            brush=QtGui.QBrush(QtCore.Qt.BrushStyle.NoBrush),
+            hoverBrush=QtGui.QBrush(QtCore.Qt.BrushStyle.NoBrush),
             swapMode="block",
         )
         self.region.sigRegionChangeFinished.connect(self.regionChanged)
@@ -77,61 +83,48 @@ class ParticleView(SinglePlotGraphicsView):
         self.legend_items.clear()
         super().clear()
 
-    def clearScatters(self) -> None:
-        for item in self.plot.listDataItems():
-            if isinstance(item, pyqtgraph.ScatterPlotItem):
-                self.plot.removeItem(item)
-
-    def clearLimits(self) -> None:
-        for limit in self.limit_items:
-            self.plot.removeItem(limit)
-        self.limit_items.clear()
-
-    def drawSignal(
+    def drawResult(
         self,
-        name: str,
-        x: np.ndarray,
-        y: np.ndarray,
+        result: SPCalProcessingResult,
         pen: QtGui.QPen | None = None,
-    ) -> None:
+        brush: QtGui.QBrush | None = None,
+        scatter_symbol: str = "t",
+        scatter_size: float = 6.0,
+    ):
         if pen is None:
             pen = QtGui.QPen(QtCore.Qt.GlobalColor.black, 1.0)
             pen.setCosmetic(True)
-
-        # optimise by removing points with 0 change in gradient
-        diffs = np.diff(y, n=2, append=0, prepend=0) != 0
-        curve = pyqtgraph.PlotCurveItem(
-            x=x[diffs], y=y[diffs], pen=pen, connect="all", skipFiniteCheck=True
-        )
-
-        self.legend_items[name] = MultipleItemSampleProxy(pen.color(), items=[curve])
-
-        self.plot.addItem(curve)
-        self.plot.legend.addItem(self.legend_items[name], name)
-
-        if self.region not in self.plot.items:
-            self.plot.addItem(self.region)
-
-        self.export_data[name] = y
-
-    def drawMaxima(
-        self,
-        name: str,
-        x: np.ndarray,
-        y: np.ndarray,
-        brush: QtGui.QBrush | None = None,
-        symbol: str = "t",
-        size: float = 6.0,
-    ) -> None:
         if brush is None:
             brush = QtGui.QBrush(QtCore.Qt.GlobalColor.red)
 
+        diffs = np.diff(result.signals, n=2, append=0, prepend=0) != 0
+        curve = pyqtgraph.PlotCurveItem(
+            x=result.times[diffs],
+            y=result.signals[diffs],
+            pen=pen,
+            connect="all",
+            skipFiniteCheck=True,
+        )
+        self.plot.addItem(curve)
+
+        maxima = detection_maxima(result.signals, result.regions)
         scatter = pyqtgraph.ScatterPlotItem(
-            x=x, y=y, size=size, symbol=symbol, pen=None, brush=brush
+            x=result.times[maxima],
+            y=result.signals[maxima],
+            size=scatter_size,
+            symbol=scatter_symbol,
+            pen=None,
+            brush=brush,
         )
         self.plot.addItem(scatter)
 
-        self.legend_items[name].addItem(scatter)
+        legend = MultipleItemSampleProxy(
+            pen.color(),
+            items=[curve, scatter],  # type: ignore , works
+        )
+
+        self.plot.legend.addItem(legend, result.isotope)
+        # self.result_items[result.isotope] = (curve, scatter)
 
     def drawLimits(
         self,
