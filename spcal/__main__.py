c782b2fd2b469cacd5b169ecf67a4dbecaf2c884
djdt <10266332+djdt@users.noreply.github.com>
2025-11-06 12:14:18 +0100

    update main again

--- spcal/__main__.py
+++ spcal/__main__.py
@@ -4,21 +4,21 @@ import logging
 import sys
 from importlib.resources import files
 from pathlib import Path
+from typing import Any
 
 from PySide6 import QtCore, QtGui, QtWidgets
 
+import spcal.resources  # noqa: F401
 from spcal.datafile import (
-    SPCalDataFile,
     SPCalNuDataFile,
-    SPCalTOFWERKDataFile,
     SPCalTextDataFile,
+    SPCalTOFWERKDataFile,
 )
+from spcal.gui.mainwindow import SPCalMainWindow
 from spcal.io.nu import is_nu_directory, is_nu_run_info_file
 from spcal.io.text import guess_text_parameters, is_text_file
 from spcal.io.tofwerk import is_tofwerk_file
 from spcal.isotope import SPCalIsotope
-from spcal.gui.mainwindow import SPCalMainWindow
-import spcal.resources  # noqa: F401
 
 logging.captureWarnings(True)
 logger = logging.getLogger("spcal")
@@ -26,14 +26,41 @@ logger.setLevel(logging.INFO)
 
 
 def parse_args(argv: list[str] | None = None) -> argparse.Namespace:
-    def existing_file(file: str) -> Path:
-        path = Path(file)
-        if not path.exists():
-            raise argparse.ArgumentTypeError(f"file {file} does not exist")
-        return path
+    class DataFileAction(argparse.Action):
+        def __call__(
+            self,
+            parser: argparse.ArgumentParser,
+            namespace: argparse.Namespace,
+            values: Any | list[Any],
+            option_string: str | None = None,
+        ):
+            if isinstance(values, list):
+                path = Path(values[0])
+            else:
+                path = Path(values)
 
-    def isotope(text: str) -> SPCalIsotope:
-        return SPCalIsotope.fromString(text)
+            if not path.exists():
+                parser.error(f"{path} does not exist")
+
+            if is_nu_directory(path) or is_nu_run_info_file(path):
+                data_file = SPCalNuDataFile.load(path)
+            elif is_tofwerk_file(path):
+                data_file = SPCalTOFWERKDataFile.load(path)
+            elif is_text_file(path):
+                delim, skip_rows, _ = guess_text_parameters(path.open().readlines(2048))
+                data_file = SPCalTextDataFile.load(
+                    path, delimiter=delim, skip_rows=skip_rows
+                )
+            else:
+                parser.error(f"{path} is not a valid single particle file")
+
+            if isinstance(values, list):
+                isotopes = [SPCalIsotope.fromString(value) for value in values[1:]]
+                data_file.selected_isotopes = isotopes
+
+            data_files = getattr(namespace, "open") or []
+            data_files.append(data_file)
+            setattr(namespace, "open", data_files)
 
     parser = argparse.ArgumentParser(
         prog="spcal",
@@ -41,39 +68,22 @@ def parse_args(argv: list[str] | None = None) -> argparse.Namespace:
     )
 
     parser.add_argument(
-        "--open", nargs="+", type=existing_file, help="open sample files"
-    )
-    parser.add_argument(
-        "--isotopes",
+        "--open",
         nargs="+",
-        type=isotope,
-        help="isotopes to select in opened files, defaults to first 5",
+        metavar=("PATH", "ISOTOPES"),
+        type=str,
+        action=DataFileAction,
+        help="open sample file and optionally select isotopes",
     )
     parser.add_argument(
         "--nohook", action="store_true", help="don't install the execption hook"
     )
-    parser.add_argument("--old-gui", action="store_true")
     parser.add_argument(
         "qtargs", nargs=argparse.REMAINDER, help="arguments to pass to Qt"
     )
     args = parser.parse_args(argv)
 
     return args
-
-
-def open_file(path: Path, isotopes: list[SPCalIsotope] | None = None) -> SPCalDataFile:
-    if is_nu_directory(path) or is_nu_run_info_file(path):
-        data_file = SPCalNuDataFile.load(path)
-    elif is_tofwerk_file(path):
-        data_file = SPCalTOFWERKDataFile.load(path)
-    elif is_text_file(path):
-        delim, skip_rows, _ = guess_text_parameters(path.open().readlines(2048))
-        data_file = SPCalTextDataFile.load(path, delimiter=delim, skip_rows=skip_rows)
-    else:
-        raise FileNotFoundError("getImportDialogForPath: invalid directory.")
-
-    data_file.selected_isotopes = isotopes or data_file.isotopes
-    return data_file
 
 
 def main(argv: list[str] | None = None) -> int:
@@ -106,9 +116,10 @@ def main(argv: list[str] | None = None) -> int:
     window.show()
 
     if args.open:
-        for path in args.open:
-            data_file = open_file(path)
-            window.addDataFile(data_file, args.isotopes or data_file.isotopes[:10])
+        for data_file in args.open:
+            window.addDataFile(
+                data_file, data_file.selected_isotopes or [data_file.isotopes[0]]
+            )
 
     # Keep event loop active with timer
     timer = QtCore.QTimer()
