f353b8186f61d40acd6c0d97d694287047ce20ac
djdt <10266332+djdt@users.noreply.github.com>
2025-11-19 16:48:23 +0100

    ?????

--- spcal/gui/dialogs/selectisotope.py
+++ spcal/gui/dialogs/selectisotope.py
@@ -1,7 +1,42 @@
 from PySide6 import QtCore, QtWidgets
 
 from spcal.datafile import SPCalDataFile
+from spcal.gui.dialogs.nontarget import NonTargetScreeningDialog
 from spcal.gui.widgets import PeriodicTableSelector
+
+
+class ScreeningOptionsDialog(QtWidgets.QDialog):
+    def __init__(self, parent: QtWidgets.QWidget | None = None):
+        super().__init__(parent)
+        self.setWindowTitle("Screening Options")
+
+        self.screening_ppm = QtWidgets.QSpinBox()
+        self.screening_ppm.setRange(0, 999999)
+        self.screening_ppm.setValue(100)
+        self.screening_ppm.setSingleStep(10)
+        self.screening_ppm.setSuffix(" ppm")
+        self.screening_ppm.setToolTip(
+            "The number of detection (as particles per million events) required to pass the screen."
+        )
+
+        self.screening_size = QtWidgets.QSpinBox()
+        self.screening_size.setRange(0, 100000000)
+        self.screening_size.setValue(1000000)
+        self.screening_size.setSingleStep(100000)
+        self.screening_size.setToolTip(
+            "Number of events to screen, a larger number takes longer."
+        )
+
+        self.button_box = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.StandardButton.Ok
+            | QtWidgets.QDialogButtonBox.StandardButton.Cancel
+        )
+
+        layout = QtWidgets.QFormLayout()
+        layout.addRow("Screening Threshold", self.screening_ppm)
+        layout.addRow("Screening Size", self.screening_size)
+        layout.addWidget(self.button_box)
+        self.setLayout(layout)
 
 
 class SelectIsotopesDialog(QtWidgets.QDialog):
@@ -22,8 +57,10 @@ class SelectIsotopesDialog(QtWidgets.QDialog):
             QtWidgets.QDialogButtonBox.StandardButton.Ok
             | QtWidgets.QDialogButtonBox.StandardButton.Cancel
         )
-        self.button_box.accepted.connect(self.accept)
-        self.button_box.rejected.connect(self.reject)
+        self.button_box.addButton(
+            "Screen", QtWidgets.QDialogButtonBox.ButtonRole.ResetRole
+        )
+        self.button_box.clicked.connect(self.onButtonClicked)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(
@@ -34,6 +71,21 @@ class SelectIsotopesDialog(QtWidgets.QDialog):
         layout.addWidget(self.table, 1)
         layout.addWidget(self.button_box, 0)
         self.setLayout(layout)
+
+    def onButtonClicked(self, button: QtWidgets.QAbstractButton):
+        sb = self.button_box.standardButton(button)
+        role = self.button_box.buttonRole(button)
+        if role == QtWidgets.QDialogButtonBox.ButtonRole.ResetRole:  # screen
+            self.dialogScreen()
+
+        if sb == QtWidgets.QDialogButtonBox.StandardButton.Ok:
+            self.accept()
+        else:
+            self.reject()
+
+    def dialogScreen(self):
+        dlg = ScreeningOptionsDialog()
+        dlg.exec()
 
     def completeChanged(self):
         complete = self.isComplete()
