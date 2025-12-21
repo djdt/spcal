5f2ba8a18d915ce7f6d5c69ec236f7e46b627016
djdt <10266332+djdt@users.noreply.github.com>
2025-12-21 10:53:11 +0100

    bump up by 10

--- spcal/gui/modelviews/__init__.py
+++ spcal/gui/modelviews/__init__.py
@@ -1,18 +1,18 @@
 from PySide6 import QtCore
 
 # Value and Unit widgets
-ValueErrorRole = QtCore.Qt.ItemDataRole.UserRole + 1
-BaseValueRole = QtCore.Qt.ItemDataRole.UserRole + 2
-BaseValueErrorRole = ValueErrorRole + 3
-CurrentUnitRole = QtCore.Qt.ItemDataRole.UserRole + 4
-UnitsRole = QtCore.Qt.ItemDataRole.UserRole + 5
-UnitLabelRole = QtCore.Qt.ItemDataRole.UserRole + 6
+ValueErrorRole = QtCore.Qt.ItemDataRole.UserRole + 10
+BaseValueRole = QtCore.Qt.ItemDataRole.UserRole + 11
+BaseValueErrorRole = ValueErrorRole + 12
+CurrentUnitRole = QtCore.Qt.ItemDataRole.UserRole + 13
+UnitsRole = QtCore.Qt.ItemDataRole.UserRole + 14
+UnitLabelRole = QtCore.Qt.ItemDataRole.UserRole + 15
 
 # Isotope
-IsotopeRole = QtCore.Qt.ItemDataRole.UserRole + 10
+IsotopeRole = QtCore.Qt.ItemDataRole.UserRole + 20
 
 # DataFile
-DataFileRole = QtCore.Qt.ItemDataRole.UserRole + 20
+DataFileRole = QtCore.Qt.ItemDataRole.UserRole + 30
 
 # Options
-IsotopeOptionRole = QtCore.Qt.ItemDataRole.UserRole + 30
+IsotopeOptionRole = QtCore.Qt.ItemDataRole.UserRole + 40
