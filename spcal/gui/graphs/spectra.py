106742d8552989021b2455c90760d2fad82d31b5
djdt <10266332+djdt@users.noreply.github.com>
2026-01-02 15:57:39 +0100

    add hover

--- spcal/gui/graphs/spectra.py
+++ spcal/gui/graphs/spectra.py
@@ -1,14 +1,52 @@
 from pathlib import Path
 import numpy as np
 from PySide6 import QtCore, QtGui, QtWidgets
+import pyqtgraph
+
+from spcal.calc import search_sorted_closest
 
 from spcal.gui.graphs.base import SinglePlotGraphicsView
-from spcal.gui.graphs.legends import HistogramItemSample
-from spcal.gui.graphs.viewbox import ViewBoxForceScaleAtZero
-from spcal.isotope import ISOTOPE_TABLE, SPCalIsotope
-from spcal.processing import SPCalProcessingResult
 from spcal.datafile import SPCalDataFile, SPCalNuDataFile, SPCalTOFWERKDataFile
-from spcal.processing.method import SPCalProcessingMethod
+
+
+class SpectraPlotItem(pyqtgraph.PlotCurveItem):
+    def __init__(
+        self,
+        xs: np.ndarray,
+        ys: np.ndarray,
+        pen: QtGui.QPen,
+        parent: QtWidgets.QGraphicsItem | None = None,
+    ):
+        xs = np.repeat(xs, 2)
+        ys = np.stack((np.zeros_like(ys), ys), axis=1).ravel()
+
+        super().__init__(xs, ys, connect="pairs", pen=pen, parent=parent)
+        self.opts["mouseWidth"] = 50.0
+
+        self.setAcceptHoverEvents(True)
+
+        self.label = pyqtgraph.TextItem(anchor=(0.5, 1))
+        self.label.setParentItem(self)
+        self.label.setVisible(False)
+
+    def hoverMoveEvent(self, event: QtWidgets.QGraphicsSceneHoverEvent):  # type: ignore
+        if self.mouseShape().contains(event.pos()):  # type: ignore
+            closest_dist = 9999.9
+            for polygon in self.getPath().toSubpathPolygons():
+                dist = QtCore.QLineF(
+                    self.mapToDevice(polygon.at(1)),  # type: ignore
+                    self.mapToDevice(event.pos()),  # type: ignore
+                ).length()
+                if dist < closest_dist:
+                    self.label.setPos(polygon.at(1))
+                    self.label.setText(f"{polygon.at(1).x():.4g}")
+                    self.label.setVisible(True)
+                    closest_dist = dist
+        else:
+            self.label.setVisible(False)
+
+    def hoverLeaveEvent(self, event: QtWidgets.QGraphicsSceneHoverEvent):
+        self.label.setVisible(False)
 
 
 class SpectraView(SinglePlotGraphicsView):
@@ -24,7 +62,6 @@ class SpectraView(SinglePlotGraphicsView):
             parent=parent,
         )
         assert self.plot.vb is not None
-        # self.plot.vb.setLimits(xMin=0.0, yMin=0.0)
 
     def spectraForTOFWERKFile(
         self, data_file: SPCalTOFWERKDataFile, regions: np.ndarray
@@ -45,7 +82,7 @@ class SpectraView(SinglePlotGraphicsView):
         data_file: SPCalDataFile,
         regions: np.ndarray,
         min_value: float = 0.5,
-        pen:QtGui.QPen|None=None,
+        pen: QtGui.QPen | None = None,
         reverse: bool = False,
     ):
         if isinstance(data_file, SPCalNuDataFile):
@@ -56,22 +93,44 @@ class SpectraView(SinglePlotGraphicsView):
             raise NotImplementedError
 
         if pen is None:
-            pen = QtGui.QPen(QtCore.Qt.GlobalColor.black,2.0*self.devicePixelRatio())
+            pen = QtGui.QPen(QtCore.Qt.GlobalColor.black, 2.0 * self.devicePixelRatio())
             pen.setCosmetic(True)
 
         mask = ys > min_value
         xs = xs[mask]
         ys = ys[mask]
 
-        xs = np.repeat(xs, 2)
-        if reverse:
-            ys *= -1.0
-        ys = np.stack((np.zeros_like(ys), ys), axis=1).ravel()
+        self.data_for_export["m/z"] = xs
+        self.data_for_export["intensity"] = ys
 
-        curve = self.drawCurve(xs, ys, pen=pen, connect="pairs")
+        curve = SpectraPlotItem(xs, ys, pen)
+        self.plot.addItem(curve)
+
+        # scatter = pyqtgraph.ScatterPlotItem(
+        #     xs,
+        #     ys,
+        #     # pen=QtGui.QPen(QtCore.Qt.PenStyle.NoPen),
+        #     # brush=QtGui.QBrush(QtCore.Qt.BrushStyle.NoBrush),
+        #     hoverable=True,
+        #     # hoverSize=100,
+        #     tip="{x:.3g} {y:.3g} {}".format,
+        # )
+        # scatter.setAcceptHoverEvents(True)
+        # self.plot.addItem(scatter)
+
+        # xs = np.repeat(xs, 2)
+        # if reverse:
+        #     ys *= -1.0
+        # ys = np.stack((np.zeros_like(ys), ys), axis=1).ravel()
+        #
+        # self.drawCurve(xs, ys, pen=pen, connect="pairs")
 
 
 if __name__ == "__main__":
+    from spcal.isotope import ISOTOPE_TABLE
+    from spcal.datafile import SPCalDataFile, SPCalNuDataFile, SPCalTOFWERKDataFile
+    from spcal.processing.method import SPCalProcessingMethod
+
     app = QtWidgets.QApplication()
 
     df = SPCalNuDataFile.load(
