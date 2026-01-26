adbbf579d5200e817399d46cf5224041e36a7d69
djdt <10266332+djdt@users.noreply.github.com>
2026-01-26 04:16:58 -0700

    selector

--- spcal/gui/graphs/scatter.py
+++ spcal/gui/graphs/scatter.py
@@ -2,10 +2,73 @@ import numpy as np
 import pyqtgraph
 from PySide6 import QtCore, QtGui, QtWidgets
 
-from spcal.cluster import prepare_results_for_clustering
 from spcal.gui.graphs.base import SinglePlotGraphicsView
 
 from spcal.processing.result import SPCalProcessingResult
+
+
+class PolygonSelectionItem(QtWidgets.QGraphicsWidget):
+    def __init__(
+        self,
+        pen: QtGui.QPen,
+        brush: QtGui.QBrush | None = None,
+        parent: QtWidgets.QGraphicsObject | None = None,
+    ):
+        super().__init__(parent)
+
+        if brush is None:
+            brush = QtGui.QBrush(QtCore.Qt.BrushStyle.NoBrush)
+
+        self.pen = pen
+        self.brush = brush
+
+        self.poly = QtGui.QPolygonF()
+
+    def boundingRect(self) -> QtCore.QRectF:
+        return self.poly.boundingRect()
+
+    def shape(self) -> QtGui.QPainterPath:
+        path = QtGui.QPainterPath()
+        path.addPolygon(self.poly)
+        return path
+
+    def addPoint(self, point: QtCore.QPointF):
+        self.poly.append(point)
+
+    def containsPoints(self, points: np.ndarray) -> np.ndarray:
+        """Test an array of points, shape (N, 2).
+
+        This is a much lazier implementation than in pewpew."""
+
+        rect = self.boundingRect()
+
+        valid = np.logical_and(
+            np.logical_and(points[:, 0] > rect.left(), points[:, 0] < rect.right()),
+            np.logical_and(points[:, 1] > rect.bottom(), points[:, 1] < rect.top()),
+        )
+        test_idx = np.flatnonzero(~valid)
+
+        valid_test_points = [
+            self.poly.containsPoint(
+                QtCore.QPointF(points[i][0], points[i][1]),
+                QtCore.Qt.FillRule.OddEvenFill,
+            )
+            for i in test_idx
+        ]
+        valid[test_idx] = valid_test_points
+        return valid
+
+    def paint(
+        self,
+        painter: QtGui.QPainter,
+        options: QtWidgets.QStyleOptionGraphicsItem,
+        widget: QtWidgets.QWidget | None = None,
+    ):
+        painter.save()
+        painter.setPen(self.pen)
+        painter.setBrush(self.brush)
+        painter.drawConvexPolygon(self.poly)
+        painter.restore()
 
 
 class ScatterView(SinglePlotGraphicsView):
@@ -13,6 +76,8 @@ class ScatterView(SinglePlotGraphicsView):
         self, font: QtGui.QFont | None = None, parent: QtWidgets.QWidget | None = None
     ):
         super().__init__("Scatter", font=font, parent=parent)
+
+        self.poly: PolygonSelectionItem| None = None
 
     def drawResults(
         self,
@@ -49,12 +114,45 @@ class ScatterView(SinglePlotGraphicsView):
         valid[result_x.peak_indicies[result_x.filter_indicies]] = True
         valid[result_y.peak_indicies[result_y.filter_indicies]] = True
 
+        self.data_for_export[f"x_{result_x.isotope}"] = x[valid]
+        self.data_for_export[f"y_{result_y.isotope}"] = y[valid]
+
         self.drawScatter(x[valid], y[valid], pen=pen, brush=brush)
 
         self.plot.xaxis.setLabel(str(result_x.isotope))
         self.plot.yaxis.setLabel(str(result_y.isotope))
 
         self.setDataLimits(-0.05, 1.05, -0.05, 1.05)
+
+    def selectedPoints(self, rect:QtCore.QRectF):
+        points = self.da
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent): # type: ignore , more pyqtgraph
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            # self.band = QtWidgets.QRubberBand(QtWidgets.QRubberBand.Shape.Rectangle, self)
+            # self.band.setGeometry(QtCore.QRect(event.pos(), QtCore.QSize(0,0)))
+            # # pen = QtGui.QPen(QtCore.Qt.GlobalColor.red, 0.0)
+            # # rb
+            # #
+            # # self.poly = PolygonSelectionItem(pen)
+            # # self.plot.addItem(self.poly)
+            # # self.poly.addPoint(event.pos())
+            self.setDragMode(QtWidgets.QGraphicsView.DragMode.RubberBandDrag)
+
+            event.accept()
+        else:
+            super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event:QtGui.QMouseEvent):
+        if event.button() == QtCore.Qt.MouseButton.LeftButton and self.band is not None:
+            self.band.setGeometry(QtCore.QRect(self.band.geometry().topLeft(), event.pos()))
+        super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event: QtGui.QMouseEvent):
+        if self.dragMode() == QtWidgets.QGraphicsView.DragMode.RubberBandDrag:
+            rect = self.rubberBandRect().normalized()
+        if self.poly is not None:
+            self.removeItem
 
     def drawFit(
         self,
