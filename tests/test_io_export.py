2e5586ce24a6562be4187f5d149f11a4ce675362
djdt <10266332+djdt@users.noreply.github.com>
2026-02-03 17:06:10 +0100

    check summary

--- tests/test_io_export.py
+++ tests/test_io_export.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import numpy as np
 
+import pytest
 from spcal.datafile import SPCalTOFWERKDataFile
 from spcal.io import export
 from spcal.isotope import ISOTOPE_TABLE
@@ -8,20 +9,8 @@ from spcal.processing import SPCalProcessingMethod
 from spcal.processing.options import SPCalIsotopeOptions
 
 
-def test_export_spcal_processing_results_known(test_data_path: Path, tmp_path: Path):
-    tmp = tmp_path.joinpath("test_export.csv")
-
-    units = {"signal": ("cts", 1.0), "mass": ("fg", 1e-18), "size": ("nm", 1e-9)}
-
-    df = SPCalTOFWERKDataFile.load(
-        test_data_path.joinpath("tofwerk/tofwerk_testdata.h5")
-    )
-    df.selected_isotopes = [
-        ISOTOPE_TABLE[("In", 115)],
-        ISOTOPE_TABLE[("Sn", 118)],
-        ISOTOPE_TABLE[("Sb", 121)],
-    ]
-
+@pytest.fixture(scope="module")
+def export_method() -> SPCalProcessingMethod:
     method = SPCalProcessingMethod()
     method.limit_options.compound_poisson_kws["sigma"] = 0.4
     method.instrument_options.uptake = 0.2e-3 / 60.0
@@ -35,11 +24,30 @@ def test_export_spcal_processing_results_known(test_data_path: Path, tmp_path: P
     method.isotope_options[ISOTOPE_TABLE[("Sb", 121)]] = SPCalIsotopeOptions(
         5.775e3, 3e6, 0.5146
     )
+    return method
 
-    results = method.processDataFile(df)
-    results = method.filterResults(results)
+
+def test_export_spcal_processing_results_known(
+    test_data_path: Path, tmp_path: Path, export_method: SPCalProcessingMethod
+):
+    tmp = tmp_path.joinpath("test_export.csv")
+
+    units = {"signal": ("cts", 1.0), "mass": ("fg", 1e-18), "size": ("nm", 1e-9)}
+
+    df = SPCalTOFWERKDataFile.load(
+        test_data_path.joinpath("tofwerk/tofwerk_testdata.h5")
+    )
+    df.selected_isotopes = [
+        ISOTOPE_TABLE[("In", 115)],
+        ISOTOPE_TABLE[("Sn", 118)],
+        ISOTOPE_TABLE[("Sb", 121)],
+    ]
+
+    results = export_method.processDataFile(df)
+    results = export_method.filterResults(results)
     clusters = {
-        key: method.processClusters(results, key) for key in ["signal", "mass", "size"]
+        key: export_method.processClusters(results, key)
+        for key in ["signal", "mass", "size"]
     }
 
     export.export_spcal_processing_results(
@@ -62,3 +70,40 @@ def test_export_spcal_processing_results_known(test_data_path: Path, tmp_path: P
                 assert np.isclose(float(tok), float(tok_checked))
             except ValueError:
                 assert tok == tok_checked
+
+
+def test_export_spcal_append_summary(
+    test_data_path: Path, tmp_path: Path, export_method: SPCalProcessingMethod
+):
+    units = {"signal": ("cts", 1.0), "mass": ("fg", 1e-18), "size": ("nm", 1e-9)}
+    df = SPCalTOFWERKDataFile.load(
+        test_data_path.joinpath("tofwerk/tofwerk_testdata.h5")
+    )
+    df.selected_isotopes = [
+        ISOTOPE_TABLE[("In", 115)],
+        ISOTOPE_TABLE[("Sn", 118)],
+        ISOTOPE_TABLE[("Sb", 121)],
+    ]
+
+    results = export_method.processDataFile(df)
+    results = export_method.filterResults(results)
+
+    tmp = tmp_path.joinpath("test_summary.csv")
+    with tmp.open("w") as fp:
+        export.append_results_summary(fp, df, list(results.values()), units)
+
+    lines = tmp.open("r").readlines()
+    lines_checked = (
+        test_data_path.joinpath("tofwerk_batch_summary_checked.csv")
+        .open("r")
+        .readlines()
+    )
+
+    assert lines[0] == "Data File,Isotope,Name,Unit,Value\n"
+
+    for line, checked in zip(lines[1:], lines_checked[1:]):
+        tok = line.strip().split(",")[1:]
+        tok_checked = checked.strip().split(",")[1:]
+        for t, tc in zip(tok[:-1], tok_checked[:-1]):
+            assert t == tc
+        assert np.isclose(float(tok[-1]), float(tok_checked[-1]), rtol=0.001)
