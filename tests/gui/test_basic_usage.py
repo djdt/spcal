9ee25a5e2a795fa27f09b1bae54e7d5798a6535c
djdt <10266332+djdt@users.noreply.github.com>
2026-02-03 21:24:35 +0100

    basic

--- tests/gui/test_basic_usage.py
+++ tests/gui/test_basic_usage.py
@@ -5,11 +5,8 @@ import pytest
 from PySide6 import QtCore
 from pytestqt.qtbot import QtBot
 
-from spcal.gui.inputs import InputWidget, ReferenceWidget
-from spcal.gui.main import SPCalWindow
+from spcal.datafile import SPCalTextDataFile
 from spcal.gui.mainwindow import SPCalMainWindow
-from spcal.gui.options import OptionsWidget
-from spcal.gui.results import ResultsWidget
 
 
 def click_though_options(qtbot: QtBot, options: OptionsWidget):
@@ -300,8 +297,94 @@ def test_spcal_single_quad_data(qtbot: QtBot, test_locales):
 
     window.resetInputs()
 
-def test_gui_no_data(qtbot: QtBot):
+
+def click_through_mainwindow(qtbot: QtBot, win: SPCalMainWindow):
+    assert win.instrument_options.options_widget.uptake.value() is None
+    qtbot.keyClick(
+        win.instrument_options.options_widget.uptake._value, QtCore.Qt.Key.Key_1
+    )
+    assert win.instrument_options.options_widget.uptake.value() == 1.0
+
+    assert win.instrument_options.options_widget.efficiency.value() is None
+    qtbot.keyClick(
+        win.instrument_options.options_widget.efficiency, QtCore.Qt.Key.Key_1
+    )
+    assert win.instrument_options.options_widget.efficiency.value() == 1.0
+    # limit options
+    assert not win.limit_options.options_widget.check_window.isChecked()
+    assert not win.limit_options.options_widget.window_size.isEnabled()
+    assert win.limit_options.options_widget.window_size.value() == 1000
+    qtbot.mouseClick(
+        win.limit_options.options_widget.check_window, QtCore.Qt.MouseButton.LeftButton
+    )
+    assert win.limit_options.options_widget.check_window.isChecked()
+    assert win.limit_options.options_widget.window_size.isEnabled()
+    qtbot.keyClick(win.limit_options.options_widget.window_size, QtCore.Qt.Key.Key_End)
+    qtbot.keyClick(
+        win.limit_options.options_widget.window_size, QtCore.Qt.Key.Key_Backspace
+    )
+    assert win.limit_options.options_widget.window_size.value() == 100
+
+    for i in range(1, win.limit_options.options_widget.limit_method.count()):
+        win.limit_options.options_widget.limit_method.setCurrentIndex(i)
+    win.limit_options.options_widget.limit_method.setCurrentIndex(0)
+    assert not win.limit_options.options_widget.check_iterative.isChecked()
+    qtbot.mouseClick(
+        win.limit_options.options_widget.check_iterative,
+        QtCore.Qt.MouseButton.LeftButton,
+    )
+    assert win.limit_options.options_widget.check_iterative.isChecked()
+
+    qtbot.keyClicks(win.limit_options.options_widget.gaussian.alpha, "1e-3")
+    assert np.isclose(win.limit_options.options_widget.gaussian.alpha.value(), 1e-3)  # type: ignore
+    assert np.isclose(win.limit_options.options_widget.gaussian.sigma.value(), 3.0902)  # type: ignore
+
+    qtbot.keyClicks(win.limit_options.options_widget.poisson.alpha, "1e-4")
+    assert np.isclose(win.limit_options.options_widget.poisson.alpha.value(), 1e-4)  # type: ignore
+
+    qtbot.keyClicks(win.limit_options.options_widget.compound.alpha, "1e-4")
+    assert np.isclose(win.limit_options.options_widget.compound.alpha.value(), 1e-4)  # type: ignore
+    qtbot.keyClick(
+        win.limit_options.options_widget.compound.lognormal_sigma, QtCore.Qt.Key.Key_1
+    )
+    assert np.isclose(
+        win.limit_options.options_widget.compound.lognormal_sigma.value(),  # type: ignore
+        1.0,
+    )
+
+    for action in win.toolbar_view.view_actions.values():
+        qtbot.mouseClick(win.toolbar_view.widgetForAction(action), QtCore.Qt.MouseButton.LeftButton)
+
+
+@pytest.mark.parametrize(
+    "test_locales",
+    [
+        QtCore.QLocale.Language.English,
+        QtCore.QLocale.Language.Spanish,
+        QtCore.QLocale.Language.German,
+    ],
+    indirect=True,
+)
+def test_gui_no_data(qtbot: QtBot, test_locales):
     win = SPCalMainWindow()
     qtbot.addWidget(win)
     with qtbot.waitExposed(win):
         win.show()
+
+    click_through_mainwindow(qtbot, win)
+
+
+def test_gui_single_quad_data(qtbot: QtBot, test_locales, test_data_path):
+    win = SPCalMainWindow()
+    qtbot.addWidget(win)
+    with qtbot.waitExposed(win):
+        win.show()
+
+    # npz = np.load(Path(__file__).parent.parent.joinpath("data/agilent_au_data.npz"))
+    # data = np.array(npz["au50nm"], dtype=[("Au", float)])
+    df = SPCalTextDataFile.load(test_data_path.joinpath("text/agilent_au50nm.csv"), skip_rows=4)
+    df.selected_isotopes = df.isotopes
+
+    win.files.addDataFile(df)
+
+    click_through_mainwindow(qtbot, win)
