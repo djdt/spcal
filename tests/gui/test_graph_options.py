fbc24c687801eae5a86490f4af6e7f148305ff91
djdt <10266332+djdt@users.noreply.github.com>
2026-02-06 16:34:30 +0100

    test opptions

--- tests/gui/test_graph_options.py
+++ tests/gui/test_graph_options.py
@@ -1,58 +1,57 @@
-from PySide6 import QtCore
 from pytestqt.qtbot import QtBot
 
 from spcal.gui.dialogs.graphoptions import (
     CompositionsOptionsDialog,
     HistogramOptionsDialog,
-    ScatterOptionsDialog,
+    SpectraOptionsDialog,
 )
 
 
 def test_composition_options_dialog(qtbot: QtBot):
-    dlg = CompositionsOptionsDialog(distance=1.0, minimum_size=100.0)
+    dlg = CompositionsOptionsDialog(minimum_size=100.0, mode="pie")
     qtbot.add_widget(dlg)
     with qtbot.wait_exposed(dlg):
         dlg.show()
 
-    assert dlg.lineedit_distance.text() == "100.0"
+    assert dlg.combo_mode.currentText() == "Pie"
     assert dlg.lineedit_size.text() == "100.0"
 
     # No change, no signal
-    with qtbot.assert_not_emitted(dlg.distanceChanged, wait=100):
+    with qtbot.assert_not_emitted(dlg.optionsChanged, wait=100):
         dlg.apply()
 
-    dlg.lineedit_distance.setText("10.0")
+    dlg.combo_mode.setCurrentText("Bar")
     dlg.lineedit_size.setText("1%")
 
-    with qtbot.wait_signals(
-        [dlg.distanceChanged, dlg.minimumSizeChanged],
+    with qtbot.wait_signal(
+        dlg.optionsChanged,
         timeout=100,
-        check_params_cbs=[lambda d: d == 0.1, lambda s: s == "1%"],
+        check_params_cb=lambda size, mode: size == "1%" and mode == "bar",
     ):
         dlg.apply()
 
     # Reset to default values
     dlg.reset()
 
-    with qtbot.wait_signals(
-        [dlg.distanceChanged, dlg.minimumSizeChanged],
+    with qtbot.wait_signal(
+        dlg.optionsChanged,
         timeout=100,
-        check_params_cbs=[lambda d: d == 0.03, lambda s: s == "5%"],
+        check_params_cb=lambda size, mode: size == "5%" and mode == "pie",
     ):
         dlg.apply()
+
     dlg.accept()
 
 
 def test_histogram_options_dialog(qtbot: QtBot):
     dlg = HistogramOptionsDialog(
-        fit="log normal",
         bin_widths={
             "signal": None,
             "mass": None,
             "size": None,
             "volume": None,
         },
-        percentile=95,
+        percentile=98.0,
         draw_filtered=False,
     )
     qtbot.add_widget(dlg)
@@ -60,10 +59,9 @@ def test_histogram_options_dialog(qtbot: QtBot):
         dlg.show()
 
     # No change, no signal
-    with qtbot.assert_not_emitted(dlg.fitChanged, wait=100):
+    with qtbot.assert_not_emitted(dlg.optionsChanged, wait=100):
         dlg.apply()
 
-    qtbot.mouseClick(dlg.radio_fit_norm, QtCore.Qt.MouseButton.LeftButton)
     dlg.width_signal.setBaseValue(100.0)
     dlg.width_mass.setBaseValue(1e-19)
     dlg.width_size.setBaseValue(1e-9)
@@ -82,66 +80,82 @@ def test_histogram_options_dialog(qtbot: QtBot):
             return False
         return True
 
-    with qtbot.wait_signals(
-        [
-            dlg.fitChanged,
-            dlg.binWidthsChanged,
-            dlg.percentileChanged,
-            dlg.drawFilteredChanged,
-        ],
+    with qtbot.wait_signal(
+        dlg.optionsChanged,
         timeout=100,
-        check_params_cbs=[
-            lambda f: f == "normal",
-            check_bin_widths,
-            lambda p: p == 90,
-            lambda b: b,
-        ],
+        check_params_cb=lambda widths, perc, draw: check_bin_widths(widths)
+        and perc == 90.0
+        and draw,
     ):
         dlg.apply()
 
     # Reset to default values
     dlg.reset()
 
-    with qtbot.wait_signals(
-        [dlg.fitChanged, dlg.binWidthsChanged, dlg.drawFilteredChanged],
+    with qtbot.wait_signal(
+        dlg.optionsChanged,
         timeout=100,
-        check_params_cbs=[
-            lambda f: f == "log normal",
-            lambda d: all(x is None for x in d.values()),
-            lambda b: not b,
-        ],
+        check_params_cb=lambda widths, perc, draw: all(
+            x is None for x in widths.values()
+        )
+        and perc == 98.0
+        and not draw,
     ):
         dlg.apply()
+
     dlg.accept()
 
 
-def test_scatter_options_dialog(qtbot: QtBot):
-    dlg = ScatterOptionsDialog(weighting="none", draw_filtered=False)
+# def test_scatter_options_dialog(qtbot: QtBot):
+#     dlg = ScatterOptionsDialog(weighting="none", draw_filtered=False)
+#     qtbot.add_widget(dlg)
+#     with qtbot.wait_exposed(dlg):
+#         dlg.show()
+#
+#     # No change, no signal
+#     with qtbot.assert_not_emitted(dlg.weightingChanged, wait=100):
+#         dlg.apply()
+#
+#     dlg.combo_weighting.setCurrentText("1/x")
+#     dlg.check_draw_filtered.setChecked(True)
+#
+#     with qtbot.wait_signals(
+#         [dlg.weightingChanged, dlg.drawFilteredChanged],
+#         timeout=100,
+#         check_params_cbs=[lambda w: w == "1/x", lambda b: b],
+#     ):
+#         dlg.apply()
+#
+#     # Reset to default values
+#     dlg.reset()
+#
+#     with qtbot.wait_signals(
+#         [dlg.weightingChanged, dlg.drawFilteredChanged],
+#         timeout=100,
+#         check_params_cbs=[lambda w: w == "none", lambda b: not b],
+#     ):
+#         dlg.apply()
+#     dlg.accept()
+
+
+def test_sepctra_options_dialog(qtbot: QtBot):
+    dlg = SpectraOptionsDialog(True)
     qtbot.add_widget(dlg)
     with qtbot.wait_exposed(dlg):
         dlg.show()
 
-    # No change, no signal
-    with qtbot.assert_not_emitted(dlg.weightingChanged, wait=100):
+    with qtbot.assert_not_emitted(dlg.optionsChanged, wait=100):
         dlg.apply()
 
-    dlg.combo_weighting.setCurrentText("1/x")
-    dlg.check_draw_filtered.setChecked(True)
+    dlg.check_subtract_background.click()
 
-    with qtbot.wait_signals(
-        [dlg.weightingChanged, dlg.drawFilteredChanged],
-        timeout=100,
-        check_params_cbs=[lambda w: w == "1/x", lambda b: b],
+    with qtbot.wait_signal(
+        dlg.optionsChanged, timeout=100, check_params_cb=lambda sub: not sub
     ):
         dlg.apply()
 
-    # Reset to default values
     dlg.reset()
 
-    with qtbot.wait_signals(
-        [dlg.weightingChanged, dlg.drawFilteredChanged],
-        timeout=100,
-        check_params_cbs=[lambda w: w == "none", lambda b: not b],
-    ):
-        dlg.apply()
+    assert dlg.check_subtract_background.isChecked()
+
     dlg.accept()
