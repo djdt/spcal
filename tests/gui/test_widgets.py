a1a0a40ac2631bb13ffdd062027acd47ba7f798f
djdt <10266332+djdt@users.noreply.github.com>
2026-02-09 18:28:06 +0100

    test for widgets units

--- tests/gui/test_widgets.py
+++ tests/gui/test_widgets.py
@@ -1,7 +1,9 @@
+import numpy as np
 from PySide6 import QtCore, QtGui, QtWidgets
 from pytestqt.qtbot import QtBot
 
 from spcal.gui.widgets.collapsablewidget import CollapsableWidget
+from spcal.gui.widgets.elidedlabel import ElidedLabel
 from spcal.gui.widgets.periodictable import PeriodicTableSelector
 from spcal.gui.widgets.units import UnitsWidget
 from spcal.gui.widgets.values import ValueWidget
@@ -9,11 +11,27 @@ from spcal.gui.widgets.values import ValueWidget
 
 def test_collapsable_widget(qtbot: QtBot):
     widget = CollapsableWidget("Test")
+    label = QtWidgets.QLabel("Label")
+    widget.setWidget(label)
 
     qtbot.addWidget(widget)
 
     with qtbot.waitExposed(widget):
         widget.show()
+
+    assert not label.isVisible()
+    qtbot.mouseClick(widget.button, QtCore.Qt.MouseButton.LeftButton)
+    assert label.isVisible()
+
+
+def test_edlided_label(qtbot: QtBot):
+    label = ElidedLabel("a very long line of text that should be elided")
+    label.setFixedWidth(50)
+
+    qtbot.addWidget(label)
+
+    with qtbot.waitExposed(label):
+        label.show()
 
 
 def test_periodic_table_selector(qtbot: QtBot):
@@ -69,6 +87,82 @@ def test_periodic_table_selector(qtbot: QtBot):
     assert pt.buttons["Cr"].indicator is None
 
 
+def test_units_widget(qtbot: QtBot):
+    w = UnitsWidget({"a": 1.0, "b": 0.1, "c": 0.01}, default_unit="c")
+    qtbot.addWidget(w)
+
+    assert w.baseValue() is None
+    assert w.value() is None
+    assert w.baseError() is None
+    assert w.error() is None
+    assert w.unit() == "c"
+    assert w.hasAcceptableInput()
+
+    with qtbot.waitSignal(w.baseValueChanged, timeout=100):
+        w.setValue(1.0)
+    assert w.baseValue() == 0.01
+
+    with qtbot.assertNotEmitted(w.baseValueChanged):
+        w.setBaseValue(0.01)
+
+    w.setUnit("b")
+    assert w.baseValue() == 0.01
+    assert np.isclose(w.value(), 0.1)  # type: ignore
+
+    w.setBaseValue(10.0)
+    assert w.value() == 100.0
+
+    w.setBestUnit()
+    assert w.unit() == "c"
+
+    w.setReadOnly(True)
+    assert w._value.isReadOnly()
+
+    assert w.isEnabled()
+    w.setEnabled(False)
+    assert not w._value.isEnabled()
+    assert not w.combo.isEnabled()
+    assert not w.isEnabled()
+
+    w.setToolTip("tip")
+    assert w._value.toolTip() == "tip"
+    assert w.combo.toolTip() == "tip"
+
+    # Make sure this does not cause a RecursionError
+    with qtbot.waitSignal(w.baseValueChanged, timeout=100):
+        w.setValue(np.nan)
+    with qtbot.assertNotEmitted(w.baseValueChanged):
+        w.setValue(None)
+
+
+def test_units_widget_error(qtbot: QtBot):
+    w = UnitsWidget({"a": 1.0, "b": 0.1, "c": 0.01}, base_value=50.0)
+    qtbot.addWidget(w)
+
+    assert w.baseError() is None
+    with qtbot.waitSignal(w.baseErrorChanged, timeout=100):
+        w.setBaseError(10.0)
+    assert w.error() == 10.0
+
+    with qtbot.assertNotEmitted(w.baseErrorChanged):
+        w.setError(10.0)
+
+    w.setUnit("b")
+    assert w.error() == 100.0
+
+    w.setBaseError(1.0)
+    assert w.error() == 10.0
+
+    w.setError(1.0)
+    assert w.baseError() == 0.1
+
+    # Make sure this does not cause a RecursionError
+    with qtbot.waitSignal(w.baseErrorChanged, timeout=100):
+        w.setError(np.nan)
+    with qtbot.assertNotEmitted(w.baseErrorChanged):
+        w.setError(None)
+
+
 def test_value_widget(qtbot: QtBot):
     w = ValueWidget()
     qtbot.addWidget(w)
@@ -80,8 +174,12 @@ def test_value_widget(qtbot: QtBot):
     assert w.text() == ""
     assert w.hasAcceptableInput()
 
-    w.setValue(0.123456789)
+    with qtbot.waitSignal(w.valueChanged, timeout=100):
+        w.setValue(0.123456789)
     assert w.value() == 0.123456789
+
+    with qtbot.assertNotEmitted(w.valueChanged):
+        w.setValue(0.123456789)
 
     w.clearFocus()
     assert w.text() == "0.123457"
@@ -103,7 +201,12 @@ def test_value_widget(qtbot: QtBot):
     w.setSigFigs(8)
     assert w.text() == "1.2321232"
 
-    w.setError(0.123456789)
+    with qtbot.waitSignal(w.errorChanged, timeout=100):
+        w.setError(0.123456789)
+
+    with qtbot.assertNotEmitted(w.errorChanged):
+        w.setError(0.123456789)
+
     # Don't know how to test the paint event
     assert w.error() == 0.123456789
     w.repaint()
@@ -118,6 +221,16 @@ def test_value_widget(qtbot: QtBot):
     assert w.value() == 4.0
     w.stepDown()  # reached min
     assert w.value() == 4.0
+
+    with qtbot.waitSignal(w.valueChanged):
+        w.setValue(None)
+    with qtbot.waitSignal(w.errorChanged):
+        w.setError(None)
+
+    with qtbot.assertNotEmitted(w.valueChanged):
+        w.setValue(None)
+    with qtbot.assertNotEmitted(w.errorChanged):
+        w.setError(None)
 
 
 def test_value_widget_step_function(qtbot: QtBot):
