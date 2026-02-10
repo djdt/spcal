c883ac538fcf926ee608d6be614becd7eb85be17
djdt <10266332+djdt@users.noreply.github.com>
2026-02-10 19:44:24 +0100

    tests for text and tofwerk batch, nu and summary need test

--- tests/gui/test_batch_wizard.py
+++ tests/gui/test_batch_wizard.py
@@ -1,11 +1,379 @@
 from pathlib import Path
 
-import numpy as np
+from PySide6 import QtCore
+
+import pytest
 from pytestqt.qtbot import QtBot
 
-from spcal.io.nu import read_directory, select_nu_signals
-from spcal.io.text import read_single_particle_file
-from spcal.io.tofwerk import read_tofwerk_file
+from spcal.gui.batch.formatpages import BatchTOFWERKWizardPage, BatchTextWizardPage
+
+from spcal.gui.batch.wizard import (
+    BatchFilesWizardPage,
+    BatchMethodWizardPage,
+    BatchRunWizardPage,
+    SPCalBatchProcessingWizard,
+)
+from spcal.isotope import ISOTOPE_TABLE
+from spcal.processing.method import SPCalProcessingMethod
+from spcal.processing.options import SPCalIsotopeOptions
+
+
+def test_batch_wizard_text(
+    tmp_path: Path,
+    test_data_path: Path,
+    default_method: SPCalProcessingMethod,
+    qtbot: QtBot,
+):
+    default_method.limit_options.limit_method = "compound poisson"
+    default_method.prominence_required = 0.5
+
+    wiz = SPCalBatchProcessingWizard(None, default_method, [])
+    qtbot.addWidget(wiz)
+    with qtbot.waitExposed(wiz):
+        wiz.show()
+
+    # Data file page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchFilesWizardPage)
+
+    assert not page.isComplete()
+
+    with pytest.raises(FileNotFoundError):
+        page.addFile(test_data_path.joinpath("tofwerk/test_tofwerk.h5"))
+    page.addFile(test_data_path.joinpath("text/tofwerk_export_au.csv"))
+    assert page.files.count() == 1
+    page.addFile(test_data_path.joinpath("text/tofwerk_export_au_bg.csv"))
+    assert page.files.count() == 2
+
+    # Test that no valid isotopes are availabled
+    page.addFile(test_data_path.joinpath("text/text_onecol.csv"))
+    assert page.files.count() == 3
+
+    wiz.next()
+
+    # Text Options page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchTextWizardPage)
+
+    assert page.table_isotopes.rowCount() == 0
+    assert not page.isComplete()
+
+    # Go back and remove file
+    wiz.back()
+    page = wiz.currentPage()
+    assert isinstance(page, BatchFilesWizardPage)
+
+    with qtbot.waitSignal(page.files.model().rowsRemoved, timeout=100):
+        qtbot.mouseClick(
+            page.files.viewport(),
+            QtCore.Qt.MouseButton.LeftButton,
+            pos=page.files.visualItemRect(page.files.item(2))
+            .marginsRemoved(QtCore.QMargins(5, 0, 0, 5))
+            .bottomLeft(),
+        )
+    assert page.files.count() == 2
+
+    wiz.next()
+
+    # Text Options page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchTextWizardPage)
+
+    assert page.isComplete()
+
+    assert len(page.selectedIsotopes()) == 1
+    assert page.table_isotopes.item(1, 1).text() == "197Au"  # type: ignore
+
+    page.table_isotopes.item(1, 1).setText("")  # type: ignore
+    assert not page.isComplete()
+    page.table_isotopes.item(1, 1).setText("197Au")  # type: ignore
+
+    # Prevent error in simulated data, dwelltime can't be read accurately
+    page.override_event_time.setChecked(True)
+
+    wiz.next()
+
+    # Method page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchMethodWizardPage)
+
+    assert page.isComplete()
+    assert page.isotope_table.isotope_model.rowCount() == 1
+
+    wiz.next()
+
+    # Run page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchRunWizardPage)
+
+    assert page.isComplete()
+    assert page.output_files.count() == 2
+
+    page.output_name.setText("%DataFile%.csv")
+    page.output_dir.setText(str(tmp_path))
+
+    assert page.isComplete()
+
+    wiz.accept()
+
+    wait = 100  # up to one second
+    while wait > 0:
+        wait -= 1
+        if page.status.text() == "Processing complete!":
+            break
+        qtbot.wait(10)
+    assert wait > 0
+
+    assert tmp_path.joinpath("tofwerk_export_au.csv").exists()
+    assert tmp_path.joinpath("tofwerk_export_au_bg.csv").exists()
+
+    wiz.close()
+
+
+def test_batch_wizard_nu(
+    tmp_path: Path,
+    test_data_path: Path,
+    default_method: SPCalProcessingMethod,
+    qtbot: QtBot,
+):
+    # default_method.limit_options.limit_method = "compound poisson"
+    # default_method.prominence_required = 0.5
+    # default_method.isotope_options[ISOTOPE_TABLE[("Ru", 101)]] = SPCalIsotopeOptions(
+    #     None, 1.0, 1.0
+    # )
+    #
+    wiz = SPCalBatchProcessingWizard(None, default_method, [])
+    qtbot.addWidget(wiz)
+    with qtbot.waitExposed(wiz):
+        wiz.show()
+    #
+    # # Data file page
+    # page = wiz.currentPage()
+    # assert isinstance(page, BatchFilesWizardPage)
+    #
+    # assert not page.isComplete()
+    #
+    # with pytest.raises(FileNotFoundError):
+    #     page.addFile(test_data_path.joinpath("tofwerk/test_tofwerk.h5"))
+    # with pytest.raises(FileNotFoundError):
+    #     page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    # page.radio_tofwerk.click()
+    # page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    # assert page.files.count() == 1
+    # page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    # assert page.files.count() == 2
+    #
+    # with qtbot.waitSignal(page.files.model().rowsRemoved, timeout=100):
+    #     qtbot.mouseClick(
+    #         page.files.viewport(),
+    #         QtCore.Qt.MouseButton.LeftButton,
+    #         pos=page.files.visualItemRect(page.files.item(1))
+    #         .marginsRemoved(QtCore.QMargins(5, 0, 0, 5))
+    #         .bottomLeft(),
+    #     )
+    # assert page.files.count() == 1
+    # assert page.isComplete()
+    #
+    # wiz.next()
+    #
+    # # TOFWERK Options page
+    # page = wiz.currentPage()
+    # assert isinstance(page, BatchTOFWERKWizardPage)
+    #
+    # assert not page.isComplete()
+    # assert len(page.table.selectedIsotopes()) == 0
+    #
+    # with qtbot.waitSignal(page.table.isotopesChanged):
+    #     qtbot.mouseClick(
+    #         page.table.buttons["Ru"],
+    #         QtCore.Qt.MouseButton.LeftButton,
+    #         QtCore.Qt.KeyboardModifier.ShiftModifier,
+    #     )
+    #
+    # assert len(page.table.selectedIsotopes()) == 5
+    # assert page.isComplete()
+    #
+    # wiz.next()
+    #
+    # # Method page
+    # page = wiz.currentPage()
+    # assert isinstance(page, BatchMethodWizardPage)
+    #
+    # assert page.isComplete()
+    #
+    # assert page.isotope_table.isotope_model.rowCount() == 5
+    # assert (
+    #     page.limit_options.limit_method.currentText()
+    #     == default_method.limit_options.limit_method.title()
+    # )
+    # assert (
+    #     page.limit_options.compound.lognormal_sigma.value()
+    #     == default_method.limit_options.compound_poisson_kws["sigma"]
+    # )
+    # assert page.limit_options.prominence_required == default_method.prominence_required
+    # assert (
+    #     page.isotope_table.isotope_model.data(
+    #         page.isotope_table.isotope_model.index(0, 2)
+    #     )
+    #     is None
+    # )
+    # assert (
+    #     page.isotope_table.isotope_model.data(
+    #         page.isotope_table.isotope_model.index(2, 2)
+    #     )
+    #     == 1.0
+    # )
+    #
+    # wiz.next()
+    #
+    # # Run page
+    # page = wiz.currentPage()
+    # assert isinstance(page, BatchRunWizardPage)
+    #
+    # assert page.isComplete()
+    # assert page.output_files.count() == 1
+    #
+    # page.output_name.setText("test_batch.csv")
+    # page.output_dir.setText(str(tmp_path))
+    #
+    # assert page.isComplete()
+    #
+    # wiz.accept()
+    #
+    # wait = 100  # up to one second
+    # while wait > 0:
+    #     wait -= 1
+    #     if page.status.text() == "Processing complete!":
+    #         break
+    #     qtbot.wait(10)
+    # assert wait > 0
+    #
+    # assert tmp_path.joinpath(page.output_name.text()).exists()
+    #
+    wiz.close()
+
+
+def test_batch_wizard_tofwerk(
+    tmp_path: Path,
+    test_data_path: Path,
+    default_method: SPCalProcessingMethod,
+    qtbot: QtBot,
+):
+    default_method.limit_options.limit_method = "compound poisson"
+    default_method.prominence_required = 0.5
+    default_method.isotope_options[ISOTOPE_TABLE[("Ru", 101)]] = SPCalIsotopeOptions(
+        None, 1.0, 1.0
+    )
+
+    wiz = SPCalBatchProcessingWizard(None, default_method, [])
+    qtbot.addWidget(wiz)
+    with qtbot.waitExposed(wiz):
+        wiz.show()
+
+    # Data file page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchFilesWizardPage)
+
+    assert not page.isComplete()
+
+    with pytest.raises(FileNotFoundError):
+        page.addFile(test_data_path.joinpath("tofwerk/test_tofwerk.h5"))
+    with pytest.raises(FileNotFoundError):
+        page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    page.radio_tofwerk.click()
+    page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    assert page.files.count() == 1
+    page.addFile(test_data_path.joinpath("tofwerk/tofwerk_testdata.h5"))
+    assert page.files.count() == 2
+
+    with qtbot.waitSignal(page.files.model().rowsRemoved, timeout=100):
+        qtbot.mouseClick(
+            page.files.viewport(),
+            QtCore.Qt.MouseButton.LeftButton,
+            pos=page.files.visualItemRect(page.files.item(1))
+            .marginsRemoved(QtCore.QMargins(5, 0, 0, 5))
+            .bottomLeft(),
+        )
+    assert page.files.count() == 1
+    assert page.isComplete()
+
+    wiz.next()
+
+    # TOFWERK Options page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchTOFWERKWizardPage)
+
+    assert not page.isComplete()
+    assert len(page.table.selectedIsotopes()) == 0
+
+    with qtbot.waitSignal(page.table.isotopesChanged):
+        qtbot.mouseClick(
+            page.table.buttons["Ru"],
+            QtCore.Qt.MouseButton.LeftButton,
+            QtCore.Qt.KeyboardModifier.ShiftModifier,
+        )
+
+    assert len(page.table.selectedIsotopes()) == 5
+    assert page.isComplete()
+
+    wiz.next()
+
+    # Method page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchMethodWizardPage)
+
+    assert page.isComplete()
+
+    assert page.isotope_table.isotope_model.rowCount() == 5
+    assert (
+        page.limit_options.limit_method.currentText()
+        == default_method.limit_options.limit_method.title()
+    )
+    assert (
+        page.limit_options.compound.lognormal_sigma.value()
+        == default_method.limit_options.compound_poisson_kws["sigma"]
+    )
+    assert page.limit_options.prominence_required == default_method.prominence_required
+    assert (
+        page.isotope_table.isotope_model.data(
+            page.isotope_table.isotope_model.index(0, 2)
+        )
+        is None
+    )
+    assert (
+        page.isotope_table.isotope_model.data(
+            page.isotope_table.isotope_model.index(2, 2)
+        )
+        == 1.0
+    )
+
+    wiz.next()
+
+    # Run page
+    page = wiz.currentPage()
+    assert isinstance(page, BatchRunWizardPage)
+
+    assert page.isComplete()
+    assert page.output_files.count() == 1
+
+    page.output_name.setText("test_batch.csv")
+    page.output_dir.setText(str(tmp_path))
+
+    assert page.isComplete()
+
+    wiz.accept()
+
+    wait = 100  # up to one second
+    while wait > 0:
+        wait -= 1
+        if page.status.text() == "Processing complete!":
+            break
+        qtbot.wait(10)
+    assert wait > 0
+
+    assert tmp_path.joinpath(page.output_name.text()).exists()
+
+    wiz.close()
 
 
 # def test_batch_export(tmp_path: Path, qtbot: QtBot):
