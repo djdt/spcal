fix up legends with font scaling etc

spcal/gui/graphs/composition.py
@@ -104,7 +104,9 @@ class CompositionView(SinglePlotGraphicsView):
             self.data_for_export[str(result.isotope) + "_std"] = stds[:, i]
 
         if isinstance(self.min_size, str) and self.min_size.endswith("%"):
-            min_size = np.count_nonzero(valid) * float(self.min_size.rstrip("%")) / 100.0
+            min_size = (
+                np.count_nonzero(valid) * float(self.min_size.rstrip("%")) / 100.0
+            )
         else:
             min_size = float(self.min_size)
 
@@ -182,10 +184,9 @@ class CompositionView(SinglePlotGraphicsView):
 
         # Add legend for each pie
         if self.plot.legend is not None:
-            fm = QtGui.QFontMetrics(self.font())
             for result, brush in zip(results, brushes):
                 self.plot.legend.addItem(
-                    FontScaledItemSample(None, fm, brush=brush),
+                    FontScaledItemSample(self.font(), None, brush=brush),
                     str(result.isotope),
                 )
 