893a769e16212a620f2b8139c92b15a1154e3270
djdt <10266332+djdt@users.noreply.github.com>
2026-06-15 15:40:11 +0200

    add size units

--- spcal/gui/dialogs/imageexport.py
+++ spcal/gui/dialogs/imageexport.py
@@ -1,4 +1,5 @@
 from PySide6 import QtCore, QtGui, QtWidgets
+import numpy as np
 
 
 def color_button(color: QtGui.QColor) -> QtWidgets.QToolButton:
@@ -63,18 +64,24 @@ class ImageExportDialog(QtWidgets.QDialog):
         elif isinstance(background_color, QtCore.Qt.GlobalColor):
             background_color = QtGui.QColor(background_color)
 
-        self.size_x = QtWidgets.QSpinBox()
+        self.size_x = QtWidgets.QDoubleSpinBox()
         self.size_x.setRange(100, 10000)
         self.size_x.setValue(size.width())
 
-        self.size_y = QtWidgets.QSpinBox()
+        self.size_y = QtWidgets.QDoubleSpinBox()
         self.size_y.setRange(100, 10000)
         self.size_y.setValue(size.height())
+
+        self.size_unit = QtWidgets.QComboBox()
+        self.size_unit.addItems(["pixel", "inch", "mm"])
+        self.size_unit.currentTextChanged.connect(self.sizeUnitChanged)
+        self._previous_unit = "pixel"
 
         size_layout = QtWidgets.QHBoxLayout()
         size_layout.addWidget(self.size_x, 1)
         size_layout.addWidget(QtWidgets.QLabel("×"), 0)
         size_layout.addWidget(self.size_y, 1)
+        size_layout.addWidget(self.size_unit, 0)
 
         self.dpi = QtWidgets.QSpinBox()
         self.dpi.setRange(96, 1200)
@@ -83,9 +90,10 @@ class ImageExportDialog(QtWidgets.QDialog):
         self.button_color = color_button(color)
 
         self.button_font = QtWidgets.QPushButton(f"{font.family()}: {font.pointSize()}")
-        self.button_font.setFont(font)
         self.button_font.setIcon(QtGui.QIcon.fromTheme("gtk-select-font"))
         self.button_font.pressed.connect(self.dialogFont)
+
+        self.selected_font = font
 
         self.button_font_color = color_button(font_color)
 
@@ -127,27 +135,57 @@ class ImageExportDialog(QtWidgets.QDialog):
         layout.addWidget(self.button_box, 2, 0, 1, 2)
         self.setLayout(layout)
 
+    def convertSizeUnit(self, unit_from: str, unit_to: str, value: float) -> float:
+        if unit_from == unit_to:
+            return value
+
+        if unit_from == "inch":
+            value = value * self.dpi.value()
+        elif unit_from == "mm":
+            value = value * self.dpi.value() / 25.4
+
+        if unit_to == "inch":
+            value = value / self.dpi.value()
+        elif unit_to == "mm":
+            value = value / self.dpi.value() * 25.4
+
+        return value
+
+    def sizeUnitChanged(self, unit: str):
+        size_x = self.convertSizeUnit(self._previous_unit, unit, self.size_x.value())
+        size_y = self.convertSizeUnit(self._previous_unit, unit, self.size_y.value())
+
+        for sb in [self.size_x, self.size_y]:
+            if unit == "inch":
+                sb.setRange(2, 100)
+                sb.setDecimals(2)
+            elif unit == "mm":
+                sb.setRange(50, 2000)
+                sb.setDecimals(0)
+            else:  # pixel
+                sb.setRange(100, 10000)
+                sb.setDecimals(0)
+
+        self.size_x.setValue(size_x)
+        self.size_y.setValue(size_y)
+        self._previous_unit = unit
+
     def dialogFont(self):
         ok, font = QtWidgets.QFontDialog.getFont(
             self.button_font.font(), self, "Graph Font"
         )
         if ok:
             self.button_font.setText(f"{font.family()}: {font.pointSize()}")
-            self.button_font.setFont(font)
+            self.selected_font = font
 
-    def imageOptions(
-        self,
-    ) -> tuple[
-        QtCore.QSize, int, QtGui.QColor, QtGui.QFont, QtGui.QColor, QtGui.QColor
-    ]:
-        return (
-            QtCore.QSize(self.size_x.value(), self.size_y.value()),
-            self.dpi.value(),
-            self.color(),
-            self.button_font.font(),
-            self.fontColor(),
-            self.backgroundColor(),
+    def imageSize(self) -> QtCore.QSize:
+        x = self.convertSizeUnit(
+            self.size_unit.currentText(), "pixel", self.size_x.value()
         )
+        y = self.convertSizeUnit(
+            self.size_unit.currentText(), "pixel", self.size_y.value()
+        )
+        return QtCore.QSize(int(x), int(y))
 
     def color(self) -> QtGui.QColor:
         effect = self.button_color.graphicsEffect()
@@ -166,6 +204,20 @@ class ImageExportDialog(QtWidgets.QDialog):
         effect = self.button_font_color.graphicsEffect()
         assert isinstance(effect, QtWidgets.QGraphicsColorizeEffect)
         return effect.color()
+
+    def imageOptions(
+        self,
+    ) -> tuple[
+        QtCore.QSize, int, QtGui.QColor, QtGui.QFont, QtGui.QColor, QtGui.QColor
+    ]:
+        return (
+            self.imageSize(),
+            self.dpi.value(),
+            self.color(),
+            self.selected_font,
+            self.fontColor(),
+            self.backgroundColor(),
+        )
 
     def accept(self):
         self.imageOptionsSelected.emit(*self.imageOptions())
