3d5cc748b2cd6a304f3a4a65dd413bda49a7ba50
djdt <10266332+djdt@users.noreply.github.com>
2026-08-02 18:11:20 +0200

    todo: simulate data

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -194,38 +194,3 @@ def test_single_particle_prominence_required(test_data_path: Path):
         x, loa, lod, prominence_required=0.1, points_required=1
     )
     assert sums.size == 1
-
-
-def test_noise_level():
-    """Tests detections / integrations stay the same when noise levels rise.
-    Going further beyond the limit of crit will start to integrate noise.
-    """
-    data = np.load(Path(__file__).parent.joinpath("data/agilent_au_data.npz"))
-    x = data["au50nm"]
-    ub = np.mean(x)
-    yc, _ = poisson.formula_c(ub, alpha=0.001)
-    detections, _ = detection.accumulate_detections(x, ub, yc + ub, integrate=True)
-
-    truth = detections.size
-    truth_mean = detections.mean()
-
-    np.random.seed(14234)
-
-    for lam in np.linspace(ub, yc, 5, endpoint=True):
-        noise = np.random.poisson(lam=lam, size=x.size)
-
-        noise_x = x + noise
-
-        noise_ub = np.mean(noise_x)
-        noise_yc, _ = poisson.formula_c(noise_ub, alpha=0.001)
-        detections, regions = detection.accumulate_detections(
-            noise_x,
-            noise_ub,
-            noise_yc + noise_ub,
-            integrate=True,
-        )
-
-        # Less than 1% change in number of detections
-        assert abs(truth - detections.size) / truth < 0.01
-        # Less than 5% change in mean detected area
-        assert abs(truth_mean - detections.mean()) / truth_mean < 0.05
